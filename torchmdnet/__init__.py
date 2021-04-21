5fae4e4725756861878b61b01a315cca412dfffc
Philipp Thölke <p.thoelke@gmx.de>
2021-04-21 21:14:19 +0200

    Reorganize and add ANI1 dataset

--- torchmdnet/__init__.py
+++ torchmdnet/__init__.py
@@ -1,2 +1,2 @@
 from .module import LNNP
-from . import models, calculators, data, utils
+from . import models, calculators, datasets, data, utils
---
+++ torchmdnet/datasets/__init__.py
@@ -0,0 +1,2 @@
+from .qm9 import QM9
+from .ani1 import ANI1
