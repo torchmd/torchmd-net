a74fa0512422db30a167f22894c43859145e3b4a
Philipp Thölke <p.thoelke@gmx.de>
2021-04-21 15:54:12 +0200

    Refactor imports

--- torchmdnet/__init__.py
+++ torchmdnet/__init__.py
@@ -1,6 +1,2 @@
 from .module import LNNP
-from .models import *
-from .data import *
-from .calculators import *
-from .utils import (train_val_test_split, make_splits,
-                    LoadFromFile, save_argparse, TestingContext)
+from . import models, calculators, data, utils
