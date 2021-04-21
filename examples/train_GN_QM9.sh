909a973afd68d504aeb6615d712bdfbd8cfa2142
Philipp Thölke <p.thoelke@gmx.de>
2021-04-21 21:55:02 +0200

    Update examples

--- examples/train_GN_QM9.sh
+++ examples/train_GN_QM9.sh
@@ -4,5 +4,5 @@ dev=1 # list CUDA devices to use
 logdir="logs" # log dir for storing checkpoints and metrics
 
 mkdir -p $logdir
-CUDA_VISIBLE_DEVICES=$dev python scripts/train.py --conf examples/graph-network.yaml --log-dir $logdir > $logdir/log 2>&1 &
+CUDA_VISIBLE_DEVICES=$dev python scripts/train.py --conf examples/graph-network.yaml --log-dir $logdir --redirect $logdir/log
 echo "Starting training... (saving logs at $logdir/log)"
