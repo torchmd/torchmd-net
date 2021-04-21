40731986ea6f0274be7bfd89c29e8a90d9c56ea2
Philipp Thölke <p.thoelke@gmx.de>
2021-04-21 14:04:10 +0200

    Add TorchMD calculator [WIP]

--- torchmdnet/__init__.py
+++ torchmdnet/__init__.py
@@ -1,5 +1,6 @@
 from .module import LNNP
 from .models import *
 from .data import *
+from .calculators import *
 from .utils import (train_val_test_split, make_splits,
                     LoadFromFile, save_argparse, TestingContext)
---
+++ torchmdnet/calculators/torchmdcalc.py
@@ -0,0 +1,18 @@
+import torch
+# from torchmdnet.models import load_model
+
+class External:
+    def __init__(self, netfile, embeddings, device='cpu'):
+        raise NotImplementedError('TODO: load_model is not yet implemented.')
+
+        self.model = load_model(netfile, device=device)
+        self.device = device
+        self.n_atoms = embeddings.size(1)
+        self.embeddings = embeddings.reshape(-1).to(device)
+        self.batch = torch.arange(embeddings.size(0), device=device).repeat_interleave(embeddings.size(1))
+        self.model.eval()
+
+    def calculate(self, pos, box):
+        pos = pos.to(self.device).type(torch.float32).reshape(-1, 3)
+        energy, forces = self.model(self.embeddings, pos, self.batch)
+        return energy.detach(), forces.reshape(-1, self.n_atoms, 3).detach()
