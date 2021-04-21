3258d6b8d92c6c81d6321cf5052e71889ce9763c
Philipp Thölke <philipp.thoelke@gmx.de>
2021-04-21 16:45:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='torchmdnet',
+    name='torchmd-net',
     version='0.1',
     packages=find_packages(),
     install_requires=requirements
