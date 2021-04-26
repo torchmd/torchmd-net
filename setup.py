1742a4a700978b3824db3c3b09b3c87b78423477
musil <felix.musil@epfl.ch>
2021-04-26 19:11:17 +0200

    add pip install

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+NAME = 'torchmdnet2'
+VERSION = '0.1'
+
+install_requires = [
+    'mdtraj',
+    'pytorch-lightning',
+    'tqdm',
+    'ase',
+    'e3nn',
+    'numpy',
+    'scipy'
+]
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=find_packages(),
+    zip_safe=True,
+    python_requires='>=3.8',
+    install_requires=install_requires,
+    )
