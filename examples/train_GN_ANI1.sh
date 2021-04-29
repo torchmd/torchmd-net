54b4221f01451a373b14cf0eb6e8249bd62780ec
Philipp Thölke <p.thoelke@gmx.de>
2021-04-29 16:40:48 +0200

    Detach training process

--- examples/train_GN_ANI1.sh
+++ examples/train_GN_ANI1.sh
@@ -4,5 +4,5 @@ dev=1 # list CUDA devices to use
 logdir="logs" # log dir for storing checkpoints and metrics
 
 mkdir -p $logdir
-CUDA_VISIBLE_DEVICES=$dev python scripts/train.py --conf examples/graph-network.yaml --dataset ANI1 --dataset-root ~/data/ani1 --log-dir $logdir --redirect $logdir/log
+CUDA_VISIBLE_DEVICES=$dev python scripts/train.py --conf examples/graph-network.yaml --dataset ANI1 --dataset-root ~/data/ani1 --log-dir $logdir --redirect $logdir/log &
 echo "Starting training... (saving logs at $logdir/log)"
