821b1ae6807b506c94ebdd8a5fadbc01ddb56a03
Philipp Thölke <p.thoelke@gmx.de>
2021-05-10 19:00:55 +0200

    Update MD17 example

--- examples/train_GN_MD17.sh
+++ examples/train_GN_MD17.sh
@@ -4,5 +4,5 @@ dev=1 # list CUDA devices to use
 logdir="logs" # log dir for storing checkpoints and metrics
 
 mkdir -p $logdir
-CUDA_VISIBLE_DEVICES=$dev python scripts/train.py --conf examples/graph-network.yaml --dataset QM9 --dataset-root ~/data/qm9 --dataset-arg energy_U0 --log-dir $logdir --redirect true &
+CUDA_VISIBLE_DEVICES=$dev python scripts/train.py --conf examples/graph-network.yaml --dataset MD17 --dataset-root ~/data/md17 --dataset-arg aspirin --standardize true --log-dir $logdir --redirect true &
 echo "Starting training... (saving logs at $logdir/log)"
