fix norm backpropagation add sparse kernel

torchmdnet2/nn/spherical_equivariants/invariants/power_spectrum.py
@@ -1,31 +1,27 @@
 import torch
 import torch.nn as nn
-import numpy as np
-
+import math
 from ..spherical_expansion import SphericalExpansion
 
 def powerspectrum(se_, nsp, nmax, lmax):
     J = se_.shape[0]
     se = se_.view((J, nsp, nmax, lmax**2))
-    ps = torch.zeros(J, nsp, nmax, nsp, nmax, lmax, dtype=se.dtype)
+    ps = torch.zeros(J, nsp, nmax, nsp, nmax, lmax, dtype=se.dtype, device=se.device)
     idx = 0
     for l in range(lmax):
         lbs = 2*l+1
         ps[..., l] = torch.sum(torch.einsum('ianl,ibml->ianbml', se[..., idx:idx+lbs], se[..., idx:idx+lbs]),
-                                   dim=5) / np.sqrt(lbs)
+                                   dim=5) / math.sqrt(lbs)
         idx += lbs
-
     ps = ps.view(J, nsp* nmax, nsp* nmax, lmax)
-    PS = torch.zeros(J, int((nsp* nmax+1)**2/2), lmax, dtype=se.dtype)
+    PS = torch.zeros(J, int((nsp* nmax+1)**2/2), lmax, dtype=se.dtype, device=se.device)
 
-    SQRT_2 = 1.4142135623730951
-    fac = SQRT_2 * torch.ones((nsp*nmax,nsp*nmax))
+    fac = math.sqrt(2.) * torch.ones((nsp*nmax,nsp*nmax))
     fac[range(nsp*nmax), range(nsp*nmax)] = 1.
     ids = [(i,j) for i in range(nsp*nmax)
                 for j in range(nsp*nmax) if j >= i]
     for ii, (i, j) in enumerate(ids):
             PS[:, ii, :] = fac[i,j]*ps[:, i, j, :]
-
     return PS.view(J, -1)
 
 class PowerSpectrum(nn.Module):
@@ -52,7 +48,6 @@ class PowerSpectrum(nn.Module):
         ci_anlm = self.se(data)
         pi_anbml = powerspectrum(ci_anlm, self.n_species,
                                         self.nmax, self.lmax+1)
-
         if self.normalize:
             return torch.nn.functional.normalize(
                 pi_anbml.view(-1, self.D), dim=1)

torchmdnet2/nn/spherical_equivariants/spherical_expansion.py
@@ -7,6 +7,7 @@ from torch_scatter import scatter
 from ..radial_basis import splined_radial_integrals
 from ..cutoff import ShiftedCosineCutoff
 from ...neighbor_list import torch_neighbor_list
+from ..math_utils import safe_norm, safe_normalization
 
 def mult_ln_lm(x_ln, x_lm):
     idx = 0
@@ -30,15 +31,6 @@ def species_dependant_reduction_ids(data, species2idx):
         ids[ii] = species2idx[sp_j]+ n_species*data.idx_i[ii]
     return ids
 
-def safe_norm(input, dim=0, eps_=1e-16):
-    eps = torch.tensor([eps_], dtype=input.dtype, device=input.device)
-    return torch.sqrt(torch.square(input).sum(dim=dim, keepdims=True) + eps) - torch.sqrt(eps)
-
-def safe_normalization(input, norms, eps_=1e-12):
-    mask = (norms > eps_).flatten()
-    out = input.clone()
-    out[mask] = input[mask] / norms[mask]
-    return out
 
 class SphericalExpansion(nn.Module):
     """