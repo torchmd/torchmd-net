b9971dd2403a13f21b4a2d727a9218f9c5fae89a
Philipp Thölke <p.thoelke@gmx.de>
2021-08-04 19:56:43 +0200

    More tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,4 @@
+import torch
 from torch.testing import assert_allclose
 from pytest import mark
 from glob import glob
@@ -15,7 +16,7 @@ from utils import create_example_batch
         recursive=True,
     ),
 )
-def test_load_model(checkpoint):
+def test_compare_forward(checkpoint):
     z, pos, _ = create_example_batch(multiple_batches=False)
     calc = External(checkpoint, z.unsqueeze(0))
     model = load_model(checkpoint, derivative=True)
@@ -25,3 +26,27 @@ def test_load_model(checkpoint):
 
     assert_allclose(e_calc, e_pred)
     assert_allclose(f_calc, f_pred.unsqueeze(0))
+
+
+@mark.parametrize(
+    "checkpoint",
+    glob(
+        join(dirname(dirname(__file__)), "examples", "pretrained", "**", "*.ckpt"),
+        recursive=True,
+    ),
+)
+def test_compare_forward_multiple(checkpoint):
+    z1, pos1, _ = create_example_batch(multiple_batches=False)
+    z2, pos2, _ = create_example_batch(multiple_batches=False)
+    calc = External(checkpoint, torch.stack([z1, z2], dim=0))
+    model = load_model(checkpoint, derivative=True)
+
+    e_calc, f_calc = calc.calculate(torch.cat([pos1, pos2], dim=0), None)
+    e_pred, f_pred = model(
+        torch.cat([z1, z2]),
+        torch.cat([pos1, pos2], dim=0),
+        torch.cat([torch.zeros(len(z1)), torch.ones(len(z2))]).long(),
+    )
+
+    assert_allclose(e_calc, e_pred)
+    assert_allclose(f_calc, f_pred.view(-1, len(z1), 3))
