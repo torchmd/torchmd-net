e32de8ef1bc59e15cbfdd1d8f0061207a41cd683
Raimondas Galvelis <r.galvelis@acellera.com>
2022-03-17 16:14:37 +0100

    Fix data loading

--- torchmdnet/datasets/ace.py
+++ torchmdnet/datasets/ace.py
@@ -14,28 +14,28 @@ class Ace(Dataset):
         # Index samples
         self.mol_indices = []
         for ifile, file_ in enumerate(self.files):
-            for mol_name in file_:
-                if 'error' in file_[mol_name]:
+            for imol, name in enumerate(file_):
+                if 'error' in file_[name]:
                     continue # Skip failed molecules
-                num_confs = len(file_[mol_name]['energy'])
+                num_confs = len(file_[name]['energy'])
                 for iconf in range(num_confs):
-                    self.mol_indices.append((ifile, mol_name, iconf))
+                    self.mol_indices.append((ifile, name, iconf))
 
     def get(self, i):
 
         # Get a molecule
-        ifile, mol_name, iconf = self.mol_indices[i]
-        mol = self.files[ifile][mol_name]
+        ifile, name, iconf = self.mol_indices[i]
+        mol = self.files[ifile][name]
 
         # Get molecular data
         return Data(
-            q=pt.from_numpy(mol.attrs['charge']),
-            s=pt.from_numpy(mol.attrs['spin']),
-            z=pt.from_numpy(mol['atomic_numbers']),
-            pos=pt.from_numpy(mol['positions'][iconf]),
-            y=pt.from_numpy(mol['energy'][iconf]),
-            dy=pt.from_numpy(mol['forces'][iconf]),
-            d=pt.from_numpy(mol['dipole_moment'][iconf]))
+            q=pt.tensor(mol.attrs['charge'], dtype=pt.int8),
+            s=pt.tensor(mol.attrs['spin'], dtype=pt.int8),
+            z=pt.tensor(mol['atomic_numbers'], dtype=pt.int8),
+            pos=pt.tensor(mol['positions'][iconf], dtype=pt.float32),
+            y=pt.tensor(mol['energy'][iconf], dtype=pt.float64),
+            dy=pt.tensor(mol['forces'][iconf], dtype=pt.float32),
+            d=pt.tensor(mol['dipole_moment'][iconf], dtype=pt.float32))
 
     def len(self):
         return len(self.mol_indices)
