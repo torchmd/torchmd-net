e90b289fc98e64132898ed9480a2e7e311553249
Raimondas Galvelis <r.galvelis@acellera.com>
2022-03-30 18:21:43 +0200

    Fix the list of source files

--- torchmdnet/neighbors/__init__.py
+++ torchmdnet/neighbors/__init__.py
@@ -2,7 +2,7 @@ import os
 import torch as pt
 from torch.utils import cpp_extension
 
-sources = ['neighbors.cpp', 'neighbors_cpu.cpp'] + ['neighbors_cuda.cu'] if pt.cuda.is_available() else []
+sources = ['neighbors.cpp', 'neighbors_cpu.cpp'] + (['neighbors_cuda.cu'] if pt.cuda.is_available() else [])
 sources = [os.path.join(os.path.dirname(__file__), name) for name in sources]
 
 cpp_extension.load(name='neighbors', sources=sources, is_python_module=False)
