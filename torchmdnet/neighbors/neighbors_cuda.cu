886fd3e7a20ac1423fb4375d1b86f3b084ead089
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2022-04-06 17:38:32 +0200

    Improve implementation of GPU neighbors calculation

--- torchmdnet/neighbors/neighbors_cuda.cu
+++ torchmdnet/neighbors/neighbors_cuda.cu
@@ -5,6 +5,9 @@
 
 namespace torch {
 static const double radius = 10;
+static const double radius_squared = radius *  radius;
+
+static const int num_threads = 128;
 
 template <typename scalar_t, int num_dims>
     using Accessor = PackedTensorAccessor32<scalar_t, num_dims, RestrictPtrTraits>;
@@ -18,21 +21,65 @@ template <typename scalar_t> __device__ __forceinline__ scalar_t sqrt_(scalar_t 
 template<> __device__ __forceinline__ float sqrt_(float x) { return ::sqrtf(x); };
 template<> __device__ __forceinline__ double sqrt_(double x) { return ::sqrt(x); };
 
-#define HASH_COORDS_TO_INDEX(x,y,z) ((num_partitions_x - 1) * ((num_partitions_y - 1) * z + y) + x)
+template <typename scalar_t> __global__ void kernel_get_max_coords(
+    const Accessor<scalar_t, 2> positions,
+    Accessor<scalar_t, 2> result
+) {
+    const int32_t index = blockIdx.x * blockDim.x + threadIdx.x;
+    const int32_t num_atoms = positions.size(0);
 
-template <typename scalar_t> __global__ void forward_kernel(
+    scalar_t max_x = 0.0;
+    scalar_t max_y = 0.0;
+    scalar_t max_z = 0.0;
+
+    for (int32_t iter = index; iter < num_atoms; iter += blockDim.x * gridDim.x) {
+        if (positions[iter][0] > max_x) {
+            max_x = positions[iter][0];
+        }
+        if (positions[iter][1] > max_y) {
+            max_y = positions[iter][1];
+        }
+        if (positions[iter][2] > max_z) {
+            max_z = positions[iter][2];
+        }
+    }
+
+    __shared__ scalar_t buf[num_threads][3];
+
+    buf[threadIdx.x][0] = max_x;
+    buf[threadIdx.x][1] = max_y;
+    buf[threadIdx.x][2] = max_z;
+
+    __syncthreads();
+
+    for (int32_t stride = blockDim.x / 2; stride > 0; stride /= 2) {
+        if (threadIdx.x < stride) {
+            if (buf[threadIdx.x + stride][0] > buf[threadIdx.x][0]) {
+                buf[threadIdx.x][0] = buf[threadIdx.x + stride][0];
+            }
+            if (buf[threadIdx.x + stride][1] > buf[threadIdx.x][1]) {
+                buf[threadIdx.x][1] = buf[threadIdx.x + stride][1];
+            }
+            if (buf[threadIdx.x + stride][2] > buf[threadIdx.x][2]) {
+                buf[threadIdx.x][2] = buf[threadIdx.x + stride][2];
+            }
+
+            __syncthreads();
+        }
+    }
+
+    if (threadIdx.x == 0) {
+        result[blockIdx.x][0] = buf[0][0];
+        result[blockIdx.x][1] = buf[0][1];
+        result[blockIdx.x][2] = buf[0][2];
+    }
+}
+
+template <typename scalar_t> __global__ void forward_kernel_get_hashes(
     const Accessor<scalar_t, 2> positions,
     const int32_t max_hash_size,
-    const int32_t num_partitions_x,
-    const int32_t num_partitions_y,
-    const int32_t num_partitions_z,
-    Accessor<int32_t, 2> geometric_hash,
-    Accessor<int32_t, 1> geometric_hash_sizes,
-    Accessor<int32_t, 1> rows,
-    Accessor<int32_t, 1> columns,
-    Accessor<scalar_t, 2> deltas,
-    Accessor<scalar_t, 1> distances,
-    int32_t *total_neighbors
+    Accessor<int32_t, 4> geometric_hash,
+    Accessor<int32_t, 3> geometric_hash_sizes
 ) {
     const int32_t index = blockIdx.x * blockDim.x + threadIdx.x;
     const int32_t num_atoms = positions.size(0);
@@ -42,49 +89,105 @@ template <typename scalar_t> __global__ void forward_kernel(
     const int32_t coord_y = round(positions[index][1] / radius) + 1;
     const int32_t coord_z = round(positions[index][2] / radius) + 1;
 
-    const int32_t hash_index = HASH_COORDS_TO_INDEX(coord_x, coord_y, coord_z);
+    const int32_t last_size = atomicAdd(&geometric_hash_sizes[coord_x][coord_y][coord_z], 1);
+    if (last_size >= max_hash_size) return;
+    geometric_hash[coord_x][coord_y][coord_z][last_size] = index;
+}
 
-    const int32_t last_size = atomicAdd(&geometric_hash_sizes[hash_index], 1);
-    if (last_size == max_hash_size) return;
-    geometric_hash[hash_index][last_size] = index;
+template <typename scalar_t> __global__ void forward_kernel_get_neighbors(
+    const Accessor<scalar_t, 2> positions,
+    const Accessor<int32_t, 4> geometric_hash,
+    const Accessor<int32_t, 3> geometric_hash_sizes,
+    Accessor<int32_t, 1> rows,
+    Accessor<int32_t, 1> columns,
+    Accessor<scalar_t, 2> deltas,
+    Accessor<scalar_t, 1> distances,
+    Accessor<int32_t, 1> neighbors_number
+) {
+    const int32_t global_index = blockIdx.x * blockDim.x + threadIdx.x;
+    const int32_t index = global_index / 14;
+
+    const int32_t num_atoms = positions.size(0);
+    if (index >= num_atoms) return;
+
+    int32_t coord_x = round(positions[index][0] / radius) + 1;
+    int32_t coord_y = round(positions[index][1] / radius) + 1;
+    int32_t coord_z = round(positions[index][2] / radius) + 1;
 
     const int32_t max_num_neighbors = distances.size(0);
 
-    __syncthreads();
+    int iter = 0;
 
-    // TODO: Minimal neighbouring hashes to check are relative positions:
-    // (1,-1,-1), (1,-1,0), (1,-1,1), (1,0,-1), (1,0,0), (1,0,1), (0,-1,-1),
-    // (0,-1,0), (0,-1,1), (0,0,-1), (0,0,0), (1,-1,1), (1,-1,0), (1,-1,1)
+    switch (global_index % 14) {
+        case 0:
+            coord_x += 1; coord_y += 1; coord_z += 1;
+            break;
+        case 1:
+            coord_x += 1; coord_y += 1; coord_z += 0;
+            break;
+        case 2:
+            coord_x += 1; coord_y += 1; coord_z += -1;
+            break;
+        case 3:
+            coord_x += 1; coord_y += 0; coord_z += 1;
+            break;
+        case 4:
+            coord_x += 1; coord_y += 0; coord_z += 0;
+            break;
+        case 5:
+            coord_x += 1; coord_y += 0; coord_z += -1;
+            break;
+        case 6:
+            coord_x += 1; coord_y += -1; coord_z += 1;
+            break;
+        case 7:
+            coord_x += 1; coord_y += -1; coord_z += 0;
+            break;
+        case 8:
+            coord_x += 1; coord_y += -1; coord_z += -1;
+            break;
+        case 9:
+            coord_x += 0; coord_y += 1; coord_z += 1;
+            break;
+        case 10:
+            coord_x += 0; coord_y += 1; coord_z += 0;
+            break;
+        case 11:
+            coord_x += 0; coord_y += 1; coord_z += -1;
+            break;
+        case 12:
+            coord_x += 0; coord_y += 0; coord_z += 1;
+            break;
+        case 13:
+            coord_x += 0; coord_y += 0; coord_z += 0;
 
-    for (int32_t iter_i = -1; iter_i <= 1; iter_i++) {
-        for (int32_t iter_j = -1; iter_j <= 1; iter_j++) {
-            for (int32_t iter_k = -1; iter_k <= 1; iter_k++) {
-                const int32_t neighbor_hash_index = HASH_COORDS_TO_INDEX(coord_x + iter_i, coord_y + iter_j, coord_z + iter_k);
-
-                for (int iter = 0; iter < geometric_hash_sizes[neighbor_hash_index]; iter++) {
-                    const scalar_t delta_x = positions[index][0] - positions[geometric_hash[neighbor_hash_index][iter]][0];
-                    const scalar_t delta_y = positions[index][1] - positions[geometric_hash[neighbor_hash_index][iter]][1];
-                    const scalar_t delta_z = positions[index][2] - positions[geometric_hash[neighbor_hash_index][iter]][2];
-
-                    const scalar_t distance = sqrt_((delta_x * delta_x) + (delta_y * delta_y) + (delta_z * delta_z));
-                    if (distance < radius) {
-                        int32_t last_size = atomicAdd(total_neighbors, 1);
-                        if (*total_neighbors >= max_num_neighbors) return;
-
-                        columns[last_size] = geometric_hash[hash_index][last_size];
-                        rows[last_size] = geometric_hash[neighbor_hash_index][iter];
-                        deltas[last_size][0] = delta_x;
-                        deltas[last_size][1] = delta_y;
-                        deltas[last_size][2] = delta_z;
-                        distances[last_size] = distance;
-                    }
-                }
+            while(index != geometric_hash[coord_x][coord_y][coord_z][iter]) {
+                iter++;
             }
+            iter++;
+
+            break;
+    }
+
+    for (; iter < geometric_hash_sizes[coord_x][coord_y][coord_z]; iter++) {
+        const scalar_t delta_x = positions[index][0] - positions[geometric_hash[coord_x][coord_y][coord_z][iter]][0];
+        const scalar_t delta_y = positions[index][1] - positions[geometric_hash[coord_x][coord_y][coord_z][iter]][1];
+        const scalar_t delta_z = positions[index][2] - positions[geometric_hash[coord_x][coord_y][coord_z][iter]][2];
+
+        const scalar_t distance_squared = (delta_x * delta_x) + (delta_y * delta_y) + (delta_z * delta_z);
+        if (distance_squared < radius_squared) {
+            int32_t last_size = atomicAdd(&neighbors_number[0], 1);
+            if (neighbors_number[0] >= max_num_neighbors) return;
+
+            columns[last_size] = index;
+            rows[last_size] = geometric_hash[coord_x][coord_y][coord_z][iter];
+            deltas[last_size][0] = delta_x;
+            deltas[last_size][1] = delta_y;
+            deltas[last_size][2] = delta_z;
+            distances[last_size] = sqrt_(distance_squared);
         }
     }
 }
-
-#undef HASH_COORDS_TO_INDEX
 
 template <typename scalar_t> __global__ void backward_kernel(
     const Accessor<int32_t, 1> rows,
@@ -124,61 +227,86 @@ public:
 
         const int num_atoms = positions.size(0);
         const int num_neighbors = num_atoms * (num_atoms - 1) / 2;
-        const int num_threads = 128;
-        const int num_blocks = (num_neighbors + num_threads - 1) / num_threads;
+        const int num_blocks = (num_atoms + num_threads + 1) / num_threads;
         const auto stream = c10::cuda::getCurrentCUDAStream(positions.get_device());
 
         const TensorOptions options = positions.options();
-        const Tensor indices = arange(0, num_neighbors, options.dtype(kInt32));
+        const Tensor indices = arange(0, num_atoms, options.dtype(kInt32));
 
         const Tensor vectors = index_select(positions, 0, indices);
 
-        Tensor max_coords = zeros({3}, kFloat64);
+        const int32_t divisions = 50;
+        const Tensor max_coords = zeros({divisions, 3}, options.dtype(kFloat32));
 
-        for(unsigned iter = 0; iter < num_atoms; iter++) {
-            const Tensor vector = vectors[iter];
+        AT_DISPATCH_FLOATING_TYPES(positions.scalar_type(), "get_max_coords_step1", [&]() {
+            const c10::cuda::CUDAStreamGuard guard(stream);
+            kernel_get_max_coords<<<divisions, num_threads, 0, stream>>>(
+                get_accessor<scalar_t, 2>(positions),
+                get_accessor<scalar_t, 2>(max_coords));
+        });
 
-            max_coords[0] = max(vector[0], max_coords[0]);
-            max_coords[1] = max(vector[1], max_coords[1]);
-            max_coords[2] = max(vector[2], max_coords[2]);
-        }
+        AT_DISPATCH_FLOATING_TYPES(positions.scalar_type(), "get_max_coords_step2", [&]() {
+            const c10::cuda::CUDAStreamGuard guard(stream);
+            kernel_get_max_coords<<<1, num_threads, 0, stream>>>(
+                get_accessor<scalar_t, 2>(max_coords),
+                get_accessor<scalar_t, 2>(max_coords));
+        });
 
-        const int num_partitions_x = ceil((max_coords[0]).item<double>() / radius) + 2;
-        const int num_partitions_y = ceil((max_coords[1]).item<double>() / radius) + 2;
-        const int num_partitions_z = ceil((max_coords[2]).item<double>() / radius) + 2;
-        const int num_partitions = num_partitions_x * num_partitions_y * num_partitions_z;
+        const int num_partitions_x = ceil((max_coords[0][0]).item<double>() / radius) + 2;
+        const int num_partitions_y = ceil((max_coords[0][1]).item<double>() / radius) + 2;
+        const int num_partitions_z = ceil((max_coords[0][2]).item<double>() / radius) + 2;
         const int max_hash_size = 500;
 
-        Tensor geometric_hash = empty({num_partitions, max_hash_size}, options.dtype(kInt32));
-        Tensor geometric_hash_sizes = zeros({num_partitions}, options.dtype(kInt32));
-        int32_t total_neighbors = 0;
+        Tensor geometric_hash = empty({num_partitions_x, num_partitions_y, num_partitions_z, max_hash_size}, options.dtype(kInt32));
+        Tensor geometric_hash_sizes = zeros({num_partitions_x, num_partitions_y, num_partitions_z}, options.dtype(kInt32));
 
+        const Tensor debug = empty({num_atoms, 8}, options.dtype(kInt32));
         const Tensor rows = empty(num_neighbors, options.dtype(kInt32));
         const Tensor columns = empty(num_neighbors, options.dtype(kInt32));
         const Tensor deltas = empty({num_neighbors, 3}, options);
         const Tensor distances = empty(num_neighbors, options);
+        const Tensor neighbors_number = empty(1, options.dtype(kInt32));
 
-        AT_DISPATCH_FLOATING_TYPES(positions.scalar_type(), "get_neighbor_list", [&]() {
+        AT_DISPATCH_FLOATING_TYPES(positions.scalar_type(), "get_neighbor_list_step1", [&]() {
             const c10::cuda::CUDAStreamGuard guard(stream);
-            forward_kernel<<<num_blocks, num_threads, 0, stream>>>(
+            forward_kernel_get_hashes<<<num_blocks, num_threads, 0, stream>>>(
                 get_accessor<scalar_t, 2>(positions),
                 max_hash_size,
-                num_partitions_x,
-                num_partitions_y,
-                num_partitions_z,
-                get_accessor<int32_t, 2>(geometric_hash),
-                get_accessor<int32_t, 1>(geometric_hash_sizes),
+                get_accessor<int32_t, 4>(geometric_hash),
+                get_accessor<int32_t, 3>(geometric_hash_sizes));
+        });
+
+        std::cout << "test1" << std::endl;
+
+        const size_t num_blocks_2 = num_blocks * 2;
+        const size_t num_threads_2 = num_threads * 7;
+
+        AT_DISPATCH_FLOATING_TYPES(positions.scalar_type(), "get_neighbor_list_step2", [&]() {
+            const c10::cuda::CUDAStreamGuard guard(stream);
+            forward_kernel_get_neighbors<<<num_blocks_2, num_threads_2, 0, stream>>>(
+                get_accessor<scalar_t, 2>(positions),
+                get_accessor<int32_t, 4>(geometric_hash),
+                get_accessor<int32_t, 3>(geometric_hash_sizes),
                 get_accessor<int32_t, 1>(rows),
                 get_accessor<int32_t, 1>(columns),
                 get_accessor<scalar_t, 2>(deltas),
                 get_accessor<scalar_t, 1>(distances),
-                &total_neighbors);
+                get_accessor<int32_t, 1>(neighbors_number));
         });
 
-        ctx->save_for_backward({rows, columns, deltas, distances});
+        std::cout << "test2" << std::endl;
+
+        const Tensor result_indices = arange(0, neighbors_number.item<int32_t>(), options.dtype(kInt32));
+
+        const Tensor result_rows = index_select(rows, 0, result_indices);
+        const Tensor result_columns = index_select(columns, 0, result_indices);
+        const Tensor result_deltas = index_select(deltas, 0, result_indices);
+        const Tensor result_distances = index_select(distances, 0, result_indices);
+
+        ctx->save_for_backward({result_rows, result_columns, result_deltas, result_distances});
         ctx->saved_data["num_atoms"] = num_atoms;
 
-        return {rows, columns, distances};
+        return {result_rows, result_columns, result_distances};
     }
 
     static autograd::tensor_list backward(autograd::AutogradContext* ctx, autograd::tensor_list grad_inputs) {
@@ -197,7 +325,7 @@ public:
         const Tensor distances = neighbors[3];
         const Tensor grad_positions = zeros({num_atoms, 3}, grad_distances.options());
 
-        AT_DISPATCH_FLOATING_TYPES(grad_distances.scalar_type(), "get_neighbor_list", [&]() {
+        AT_DISPATCH_FLOATING_TYPES(grad_distances.scalar_type(), "backward_get_neighbor_list", [&]() {
             const c10::cuda::CUDAStreamGuard guard(stream);
             backward_kernel<<<num_blocks, num_threads, 0, stream>>>(
                 get_accessor<int32_t, 1>(rows),
