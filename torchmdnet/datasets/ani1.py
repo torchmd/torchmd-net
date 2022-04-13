60148fe8f5508de55b77281efdd7494f19000a60
Raimondas Galvelis <r.galvelis@acellera.com>
2022-04-13 14:19:08 +0200

    Impement the ANI-1 dataset with H5Py

--- torchmdnet/datasets/ani1.py
+++ torchmdnet/datasets/ani1.py
@@ -1,82 +1,100 @@
-import os
-from os.path import join
-from tqdm import tqdm
-from urllib import request
-import torch
-from torch_geometric.data import InMemoryDataset, extract_tar, Data
 import h5py
+import os
+import torch as pt
+from torch_geometric.data import Data, Dataset, download_url, extract_tar 
+from tqdm import tqdm
 
 
-class ANI1(InMemoryDataset):
+class ANI1(Dataset):
 
-    raw_url = "https://ndownloader.figshare.com/files/9057631"
+    HARTREE_TO_EV = 27.211386246
 
-    element_numbers = {"H": 1, "C": 6, "N": 7, "O": 8}
-
-    HAR2EV = 27.211386246
-
-    self_energies = {
-        "H": -0.500607632585 * HAR2EV,
-        "C": -37.8302333826 * HAR2EV,
-        "N": -54.5680045287 * HAR2EV,
-        "O": -75.0362229210 * HAR2EV,
+    raw_url = 'https://ndownloader.figshare.com/files/9057631'
+    atomic_numbers = {b'H': 1, b'C': 6, b'N': 7, b'O': 8}
+    atomic_energies = {
+        'H': -0.500607632585 * HARTREE_TO_EV,
+        'C': -37.8302333826 * HARTREE_TO_EV,
+        'N': -54.5680045287 * HARTREE_TO_EV,
+        'O': -75.0362229210 * HARTREE_TO_EV,
     }
 
-    def __init__(self, root, transform=None, pre_transform=None, **kwargs):
-        super(ANI1, self).__init__(root, transform, pre_transform)
-        self.data, self.slices = torch.load(self.processed_paths[0])
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, transform, pre_transform, pre_filter)
 
     @property
     def raw_file_names(self):
-        return [f"ANI-1_release/ani_gdb_s{i + 1:02d}.h5" for i in range(8)]
+        return [os.path.join('ANI-1_release', f'ani_gdb_s{i:02d}.h5') for i in range(1, 9)]
 
     @property
     def processed_file_names(self):
-        return ["ani1.pt"]
+        return ['ani-1.h5']
 
     def download(self):
-        raw_archive = join(self.raw_dir, "ANI1_release.tar.gz")
-        print(f"Downloading {self.raw_url}")
-        request.urlretrieve(self.raw_url, raw_archive)
-        extract_tar(raw_archive, self.raw_dir)
-        os.remove(raw_archive)
+        archive = download_url(self.raw_url, self.raw_dir)
+        extract_tar(archive, self.raw_dir)
+        os.remove(archive)
 
     def process(self):
-        data_list = []
-        for path in tqdm(self.raw_paths, desc="raw h5 files"):
-            data = h5py.File(path, "r")
-            for file_name in data:
-                for molecule_name in tqdm(
-                    data[file_name], desc="molecules", leave=False
-                ):
-                    group = data[file_name][molecule_name]
-                    elements = torch.tensor(
-                        [
-                            self.element_numbers[str(elem)[-2]]
-                            for elem in group["species"]
-                        ]
-                    )
-                    positions = torch.from_numpy(group["coordinates"][:])
-                    energies = torch.from_numpy(
-                        group["energies"][:] * self.HAR2EV
-                    ).float()
 
-                    elements = elements.expand(positions.size(0), -1)
-                    for z, pos, energy in zip(elements, positions, energies):
-                        data_list.append(Data(z=z, pos=pos, y=energy.view(1, 1)))
+        tmp_file = self.processed_paths[0] + '.tmp'
+        with h5py.File(tmp_file, 'w', driver='stdio') as h5:
 
-        if self.pre_filter is not None:
-            data_list = [data for data in data_list if self.pre_filter(data)]
+            h5.create_dataset('z', (0,), chunks=(1,), maxshape=(None,),
+                              dtype=h5py.vlen_dtype('int8'), compression=None)
+            h5.create_dataset('pos', (0,), chunks=(1,), maxshape=(None,),
+                              dtype=h5py.vlen_dtype('float32'), compression=None)
+            h5.create_dataset('y', (0,), chunks=(1,), maxshape=(None,),
+                              dtype=h5py.vlen_dtype('float64'), compression=None)
 
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(data) for data in data_list]
+            for path in tqdm(self.raw_paths[7:], desc='Files'):
+                molecules = list(h5py.File(path).values())[0].values()
 
-        data, slices = self.collate(data_list)
-        torch.save((data, slices), self.processed_paths[0])
+                for mol in tqdm(molecules, desc='Molecules', leave=False):
+                    z = pt.tensor([self.atomic_numbers[atom] for atom in mol['species']])
+                    all_pos = pt.tensor(mol['coordinates'][:])
+                    all_y = pt.tensor(mol['energies'][:] * self.HARTREE_TO_EV)
+
+                    assert all_pos.shape[0] == all_y.shape[0]
+                    assert all_pos.shape[1] == z.shape[0]
+                    assert all_pos.shape[2] == 3
+
+                    for pos, y in tqdm(zip(all_pos, all_y), desc='Conformers',
+                                       total=len(all_pos), leave=False):
+                        data = Data(z=z, pos=pos, y=y.view(1, 1))
+
+                        if self.pre_filter is not None and not self.pre_filter(data):
+                            continue
+
+                        if self.pre_transform is not None:
+                            data = self.pre_transform(data)
+
+                        i = len(h5['z'])
+                        h5['z'].resize(i + 1, axis=0)
+                        h5['pos'].resize(i + 1, axis=0)
+                        h5['y'].resize(i + 1, axis=0)
+
+                        h5['z'][i] = data.z
+                        h5['pos'][i] = data.pos.flatten().numpy()
+                        h5['y'][i] = data.y
+
+        os.rename(tmp_file, self.processed_paths[0])
+
+    def len(self):
+        with h5py.File(self.processed_paths[0]) as h5:
+            return len(h5['z'])
+
+    def get(self, idx):
+
+        with h5py.File(self.processed_paths[0]) as h5:
+            z = pt.tensor(h5['z'][idx])
+            pos = pt.tensor(h5['pos'][idx]).view(-1, 3)
+            y = pt.tensor(h5['y'][idx]).view(1, 1)
+
+        return Data(z=z, pos=pos, y=y)
 
     def get_atomref(self, max_z=100):
+
         out = torch.zeros(max_z)
-        out[list(self.element_numbers.values())] = torch.tensor(
-            list(self.self_energies.values())
-        )
+        out[list(self.atomic_numbers.values())] = torch.tensor(list(self.atomic_energies.values()))
+
         return out.view(-1, 1)
