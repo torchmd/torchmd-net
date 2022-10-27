c59f3de76fa223b7ff6612c78f62e37fb72cd1cf
Raimondas Galvelis <r.galvelis@acellera.com>
2022-10-27 18:00:16 +0200

    Convert units and add some docs

--- torchmdnet/priors/d2.py
+++ torchmdnet/priors/d2.py
@@ -5,9 +5,19 @@ from torch_scatter import scatter
 
 
 class D2(BasePrior):
-    """ """
+    """Dispersive correction term as used in DFT-D2
 
-    # TODO convert J/mol*nm^6
+    Units:
+      - Atomic postions: Å
+      - Energies: eV
+
+    Reference: Grimme, Stefan. "Semiempirical GGA-type density functional
+               constructed with a long‐range dispersion correction."
+               Journal of computational chemistry 27.15 (2006): 1787-1799.
+               https://onlinelibrary.wiley.com/doi/10.1002/jcc.20495
+    """
+
+    # Taken from Table 1. Units: J/mol*nm^6
     C_6 = [
         pt.nan,  # 0
         0.14,  # 1 H
@@ -20,8 +30,11 @@ class D2(BasePrior):
         0.70,  # 8 O
         0.75,  # 9 F
         0.63,  # 10 Ne
+        # Values up to Xe are available
     ]
+    C_6 = pt.tensor(C_6) * 1.03643e-5 * 10**6  # J/mol*nm^6 --> eV*Å^6
 
+    # Taken from Table 1. Units: Å
     R_r = [
         pt.nan,  # 0
         1.001,  # 1 H
@@ -34,6 +47,7 @@ class D2(BasePrior):
         1.342,  # 8 O
         1.287,  # 9 F
         1.243,  # 10 Ne
+        # Values up to Xe are available
     ]
 
     def __init__(self):
@@ -43,8 +57,11 @@ class D2(BasePrior):
             cutoff_lower=0, cutoff_upper=12.0, max_num_neighbors=128
         )
 
+        # Atomic parameters
         self.register_buffer("C_6", self.C_6)
         self.register_buffer("R_r", self.R_r)
+
+        # Default values from the reference
         self.d = 20
         self.s_6 = 1
 
@@ -57,7 +74,7 @@ class D2(BasePrior):
         R_r = self.R_r[Z].sum(dim=0)
 
         f_dump = 1 / (1 + pt.exp(-self.d * (R_ij / R_r - 1)))
-        E_ij = C_6 / R_ij.pow(6) * f_dump
+        E_ij = C_6 / R_ij**6 * f_dump
         E_disp = -self.s_6 * scatter(E_ij, batch, dim=0, reduce="sum")
 
         return y + E_disp
