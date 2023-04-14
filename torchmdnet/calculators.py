b98893629c2c9e2b9a4bb8124b797702f77729b1
Antonio <antonio.mirarchi@upf.edu>
2023-04-14 09:52:10 +0200

    preset transforms

--- torchmdnet/calculators.py
+++ torchmdnet/calculators.py
@@ -1,15 +1,32 @@
 import torch
 from torchmdnet.models.model import load_model
 
+# dict of preset transforms
+tranforms = {
+    "eV/A": lambda energy, forces: (
+        energy * 23.0609,
+        forces * 23.0609,
+    ),  # eV->kcal/mol, eV/A -> kcal/mol/A
+    "Hartree/Bohr": lambda energy, forces: (
+        energy * 627.509,
+        forces * 627.509 / 0.529177,
+    ),  # Hartree -> kcal/mol, Hartree/Bohr -> kcal/mol/A
+    "Haetree/A": lambda energy, forces: (
+        energy * 627.509,
+        forces * 627.509,
+    ),  # Hartree -> kcal/mol, Hartree/A -> kcal/mol/A
+}
+
+
 class External:
-    '''
+    """
     The External class is used to calculate the energy and forces of an external potential, such as a neural network. The class is initialized with the path to the neural network
-    ckpt, the embeddings, the device on which the neural network should be run and the output_transform argument.  
-    The output_transform is used to give a function that transform the energy and the forces. This is could be useful to convert the energy and the forces from the units of the 
-    neural network (the ones used to trained it) to the units of the simulation. The output_transform argument should be a string that can be evaluated to a function. 
-    The function should take two arguments, the energy and the forces, and return the transformed energy and the transformed forces.
-    '''
-    
+    ckpt, the embeddings, the device on which the neural network should be run and the output_transform argument. The output_transform is used to give a function that transform
+    the energy and the forces, this could be a preset transform or a custom function. In this way there is no constraint to the units of the neural network, the user can choose
+    the units of the simulation and the neural network will be automatically converted to the units of the simulation. The function should take two arguments, the energy and the
+    forces, and return the transformed energy and the transformed forces.
+    """
+
     def __init__(self, netfile, embeddings, device="cpu", output_transform=None):
         self.model = load_model(netfile, device=device, derivative=True)
         self.device = device
@@ -21,7 +38,12 @@ class External:
         self.model.eval()
 
         if not output_transform:
-            self.output_transformer = lambda energy, forces: (energy, forces)  # identity
+            self.output_transformer = lambda energy, forces: (
+                energy,
+                forces,
+            )  # identity
+        elif output_transform in tranforms.keys():
+            self.output_transformer = tranforms[output_transform]
         else:
             self.output_transformer = eval(output_transform)
 
