0700aae5afb2ef5d38d93a106d0fed7b3c0bf981
RaulPPealez <raulppelaez@gmail.com>
2023-05-02 23:38:11 +0200

    Working implementation

--- torchmdnet/neighbors/neighbors_cuda_cell.cu
+++ torchmdnet/neighbors/neighbors_cuda_cell.cu
@@ -1,6 +1,6 @@
+#include <ATen/cuda/CUDAContext.h>
 #include <algorithm>
 #include <c10/cuda/CUDAGuard.h>
-#include <ATen/cuda/CUDAContext.h>
 #include <c10/cuda/CUDAStream.h>
 #include <thrust/device_vector.h>
 #include <thrust/iterator/discard_iterator.h>
@@ -127,9 +127,9 @@ __host__ __device__ int3 getNumberCells(float3 box_size, float cutoff) {
 // Get the cell coordinates of a point
 __device__ int3 getCell(float3 p, float3 box_size, float cutoff) {
     p = takeToUnitCell(p, box_size);
-    int cx = floorf(p.x / cutoff);
-    int cy = floorf(p.y / cutoff);
-    int cz = floorf(p.z / cutoff);
+    int cx = floorf((p.x + float(0.5) * box_size.x) / cutoff);
+    int cy = floorf((p.y + float(0.5) * box_size.y) / cutoff);
+    int cz = floorf((p.z + float(0.5) * box_size.z) / cutoff);
     int3 cell_dim = getNumberCells(box_size, cutoff);
     if (cx == cell_dim.x)
         cx = 0;
@@ -155,9 +155,9 @@ __global__ void assignHash(const Accessor<scalar_t, 2> positions, uint64_t* hash
     auto ci = getCell(pi, box_size, cutoff);
     // Calculate the hash
     const int32_t hash = hashMorton(ci);
-    // Create a hash combining the Morton hash and the batch index, so that atoms in the same batch
+    // Create a hash combining the Morton hash and the batch index, so that atoms in the same cell
     // are contiguous
-    const int64_t hash_final = (static_cast<int64_t>(i_batch) << 32) | hash;
+    const int64_t hash_final = (static_cast<int64_t>(hash) << 32) | i_batch;
     hash_keys[i_atom] = hash_final;
     hash_values[i_atom] = i_atom;
 }
@@ -169,10 +169,11 @@ public:
     CudaAllocator() {
     }
     T* allocate(std::ptrdiff_t num_elements) {
-        return static_cast<T*>(at::cuda::getCUDADeviceAllocator()->raw_allocate(num_elements * sizeof(T)));
+        return static_cast<T*>(
+            at::cuda::getCUDADeviceAllocator()->raw_allocate(num_elements * sizeof(T)));
     }
     void deallocate(T* ptr, size_t) {
-      at::cuda::getCUDADeviceAllocator()->raw_deallocate(ptr);
+        at::cuda::getCUDADeviceAllocator()->raw_deallocate(ptr);
     }
 };
 
@@ -192,14 +193,6 @@ static auto sortPositionsByHash(const Tensor& positions, const Tensor& batch, fl
             get_accessor<int32_t, 1>(hash_values), get_accessor<int32_t, 1>(batch), box_size,
             cutoff, num_atoms);
     });
-
-    std::cout << "hash_values: " << hash_values << std::endl;
-    std::cout << "hash_keys: " << std::endl;
-    for (int i = 0; i < num_atoms; i++) {
-      uint64_t hi = hash_keys[i];
-      std::cout << std::bitset<64>(hi) << std::endl;
-    }
-
     // Sort positions by hash_values using thrust
     thrust::device_ptr<int32_t> index_ptr(hash_values.data_ptr<int32_t>());
     // pytorch allocator
@@ -207,14 +200,6 @@ static auto sortPositionsByHash(const Tensor& positions, const Tensor& batch, fl
     // Adapted for thrust
     thrust::sort_by_key(thrust::cuda::par.on(stream), hash_keys.begin(), hash_keys.end(),
                         index_ptr);
-    std::cout << "sorted hash_values: " << hash_values << std::endl;
-    // Print values of hash_keys in binary
-    std::cout << "sorted hash_keys: " << std::endl;
-    for (int i = 0; i < num_atoms; i++) {
-      uint64_t hi = hash_keys[i];
-      std::cout << std::bitset<64>(hi) << std::endl;
-    }
-
     Tensor sorted_positions = positions.index_select(0, hash_values);
     return std::make_tuple(sorted_positions, hash_values);
 }
@@ -225,13 +210,13 @@ __device__ int getCellIndex(int3 cell, int3 cell_dim) {
 
 template <typename scalar_t>
 __global__ void fillCellOffsetsD(const Accessor<scalar_t, 2> sorted_positions,
-				 const Accessor<int32_t, 1> sorted_indices,
-                                 Accessor<int32_t, 2> cell_start, Accessor<int32_t, 2> cell_end,
+                                 const Accessor<int32_t, 1> sorted_indices,
+                                 Accessor<int32_t, 1> cell_start, Accessor<int32_t, 1> cell_end,
                                  const Accessor<int32_t, 1> batch, float3 box_size, float cutoff) {
     const int32_t i_atom = blockIdx.x * blockDim.x + threadIdx.x;
     if (i_atom >= sorted_positions.size(0))
         return;
-    const int32_t i_batch = batch[sorted_indices[i_atom]];
+    // const int32_t i_batch = batch[sorted_indices[i_atom]];
     const float3 pi = make_float3(sorted_positions[i_atom][0], sorted_positions[i_atom][1],
                                   sorted_positions[i_atom][2]);
     const int3 cell_dim = getNumberCells(box_size, cutoff);
@@ -246,46 +231,34 @@ __global__ void fillCellOffsetsD(const Accessor<scalar_t, 2> sorted_positions,
         im1_cell = 0;
     }
     if (icell != im1_cell || i_atom == 0) {
-      int n_cells = cell_start.size(0);
-      if(icell>=n_cells or im1_cell>=n_cells) {
-	printf("icell: %d, im1_cell: %d, n_cells: %d\n", icell, im1_cell, n_cells);
-	return;
-      }
-        cell_start[icell][i_batch] = i_atom;
+        int n_cells = cell_start.size(0);
+        cell_start[icell] = i_atom;
         if (i_atom > 0)
-            cell_end[im1_cell][i_batch] = i_atom;
+            cell_end[im1_cell] = i_atom;
     }
-    if(i_atom == sorted_positions.size(0) - 1) {
-      cell_end[icell][i_batch] = i_atom + 1;
+    if (i_atom == sorted_positions.size(0) - 1) {
+        cell_end[icell] = i_atom + 1;
     }
 }
 
 // Fill the cell offsets for each batch, identifying the start and end of each cell for each batch
 // in the sorted positions
-static auto fillCellOffsets(const Tensor& sorted_positions,
-			    const Tensor& sorted_indices,
-			    const Tensor& batch, float3 box_size,
-                            float cutoff) {
+static auto fillCellOffsets(const Tensor& sorted_positions, const Tensor& sorted_indices,
+                            const Tensor& batch, float3 box_size, float cutoff) {
     const TensorOptions options = sorted_positions.options();
-    const int num_batches = batch[-1].item<int32_t>() + 1;
     const int3 cell_dim = getNumberCells(box_size, cutoff);
     const int num_cells = cell_dim.x * cell_dim.y * cell_dim.z;
-    const Tensor cell_start = full({num_cells, num_batches}, -1, options.dtype(torch::kInt));
-    const Tensor cell_end = empty({num_cells, num_batches}, options.dtype(torch::kInt));
-    std::cerr<<"num_cells: "<<num_cells<<std::endl;
-    std::cerr<<"num_batches: "<<num_batches<<std::endl;
+    const Tensor cell_start = full({num_cells}, -1, options.dtype(torch::kInt));
+    const Tensor cell_end = empty({num_cells}, options.dtype(torch::kInt));
     const int threads = 128;
     const int blocks = (sorted_positions.size(0) + threads - 1) / threads;
     AT_DISPATCH_FLOATING_TYPES(sorted_positions.scalar_type(), "fillCellOffsets", [&] {
         auto stream = at::cuda::getCurrentCUDAStream();
         fillCellOffsetsD<<<blocks, threads, 0, stream>>>(
-            get_accessor<scalar_t, 2>(sorted_positions),
-	    get_accessor<int32_t, 1>(sorted_indices),
-	    get_accessor<int32_t, 2>(cell_start),
-            get_accessor<int32_t, 2>(cell_end), get_accessor<int32_t, 1>(batch), box_size, cutoff);
+            get_accessor<scalar_t, 2>(sorted_positions), get_accessor<int32_t, 1>(sorted_indices),
+            get_accessor<int32_t, 1>(cell_start), get_accessor<int32_t, 1>(cell_end),
+            get_accessor<int32_t, 1>(batch), box_size, cutoff);
     });
-    std::cerr<<"cell_start: "<<cell_start<<std::endl;
-    std::cerr<<"cell_end: "<<cell_end<<std::endl;
     return std::make_tuple(cell_start, cell_end);
 }
 
@@ -312,7 +285,7 @@ template <typename scalar_t>
 __global__ void
 forward_kernel(const Accessor<scalar_t, 2> sorted_positions,
                const Accessor<int32_t, 1> original_index, const Accessor<int32_t, 1> batch,
-               const Accessor<int32_t, 2> cell_start, const Accessor<int32_t, 2> cell_end,
+               const Accessor<int32_t, 1> cell_start, const Accessor<int32_t, 1> cell_end,
                Accessor<int32_t, 2> neighbors, Accessor<scalar_t, 2> deltas,
                Accessor<scalar_t, 1> distances, Accessor<int32_t, 1> i_curr_pair, int num_atoms,
                int num_pairs, float3 box_size, float cutoff) {
@@ -337,14 +310,16 @@ forward_kernel(const Accessor<scalar_t, 2> sorted_positions,
         cell_j.z += i / 9 - 1;
         cell_j = getPeriodicCell(cell_j, cell_dim);
         int icellj = getCellIndex(cell_j, cell_dim);
-        const int firstParticle = cell_start[icellj][i_batch];
+        const int firstParticle = cell_start[icellj];
         if (firstParticle != -1) { // Continue only if there are particles in this cell
             // Index of the last particle in the cell's list
-            const int lastParticle = cell_end[icellj][i_batch];
+            const int lastParticle = cell_end[icellj];
             const int nincell = lastParticle - firstParticle;
             for (int j = 0; j < nincell; j++) {
                 int cur_j = j + firstParticle;
-                if (cur_j < i_atom) {
+                int orj = original_index[cur_j];
+                int j_batch = batch[orj];
+                if (cur_j < i_atom and j_batch == i_batch) {
                     float3 pj = make_float3(sorted_positions[cur_j][0], sorted_positions[cur_j][1],
                                             sorted_positions[cur_j][2]);
                     const scalar_t dx = pi.x - pj.x;
@@ -394,32 +369,17 @@ public:
                                        box_size[2].item<float>());
         float cutoff_ = cutoff.toFloat();
         Tensor sorted_positions, hash_values;
-        std::cerr << "before sortPositionsByHash" << std::endl;
         std::tie(sorted_positions, hash_values) =
             sortPositionsByHash(positions, batch, box_size_, cutoff_);
         cudaDeviceSynchronize();
-        std::cerr << "after sortPositionsByHash" << std::endl;
-        // Step 3
         Tensor cell_start, cell_end;
-        std::cerr << "before fillCellOffsets" << std::endl;
         std::tie(cell_start, cell_end) =
-	  fillCellOffsets(sorted_positions, hash_values,
-			  batch, box_size_, cutoff_);
-        cudaDeviceSynchronize();
-        std::cerr << "after fillCellOffsets" << std::endl;
-	cudaDeviceSynchronize();
-	std::cerr << "Number of pairs: " << num_pairs << std::endl;
-	cudaDeviceSynchronize();
-	std::cerr <<"Allocating memory for neighbors" << std::endl;
+            fillCellOffsets(sorted_positions, hash_values, batch, box_size_, cutoff_);
         const Tensor neighbors = full({2, num_pairs}, -1, options.dtype(kInt32));
-	std::cerr << "Allocating memory for deltas" << std::endl;
         const Tensor deltas = empty({num_pairs, 3}, options);
-	std::cerr << "Allocating memory for distances" << std::endl;
         const Tensor distances = full(num_pairs, 0, options);
         const Tensor i_curr_pair = zeros(1, options.dtype(kInt32));
-        cudaDeviceSynchronize();
-        std::cerr << "before forward_kernel" << std::endl;
-	const auto stream = getCurrentCUDAStream(positions.get_device());
+        const auto stream = getCurrentCUDAStream(positions.get_device());
         { // Use the cell list for each batch to find the neighbors
             const CUDAStreamGuard guard(stream);
             AT_DISPATCH_FLOATING_TYPES(positions.scalar_type(), "forward", [&] {
@@ -428,30 +388,24 @@ public:
                 forward_kernel<<<blocks, threads, 0, stream>>>(
                     get_accessor<scalar_t, 2>(sorted_positions),
                     get_accessor<int32_t, 1>(hash_values), get_accessor<int32_t, 1>(batch),
-                    get_accessor<int32_t, 2>(cell_start), get_accessor<int32_t, 2>(cell_end),
+                    get_accessor<int32_t, 1>(cell_start), get_accessor<int32_t, 1>(cell_end),
                     get_accessor<int32_t, 2>(neighbors), get_accessor<scalar_t, 2>(deltas),
                     get_accessor<scalar_t, 1>(distances), get_accessor<int32_t, 1>(i_curr_pair),
                     num_atoms, num_pairs, box_size_, cutoff_);
             });
         }
-        cudaDeviceSynchronize();
-        std::cerr << "after forward_kernel" << std::endl;
         // Synchronize and check the number of pairs found. Note that this is incompatible with CUDA
         // graphs
         if (checkErrors) {
-	  std::cout<<"checking errors"<<std::endl;
             int num_found_pairs = i_curr_pair[0].item<int32_t>();
             TORCH_CHECK(num_found_pairs <= max_num_pairs_,
                         "Too many neighbor pairs found. Maximum is " +
                             std::to_string(max_num_pairs_),
                         " but found " + std::to_string(num_found_pairs));
-	    std::cout<<"no errors"<<std::endl;
         }
-	std::cout<<"before resize"<<std::endl;
         neighbors.resize_({2, i_curr_pair[0].item<int32_t>()});
         deltas.resize_({i_curr_pair[0].item<int32_t>(), 3});
         distances.resize_(i_curr_pair[0].item<int32_t>());
-	std::cout<<"after resize"<<std::endl;
         ctx->save_for_backward({neighbors, deltas, distances});
         ctx->saved_data["num_atoms"] = num_atoms;
         return {neighbors, deltas, distances};
