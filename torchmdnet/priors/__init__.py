237b4fe1bf0ce52721b42fe51f35efca018753f9
Peter Eastman <peastman@stanford.edu>
2023-06-07 10:54:53 -0700

    Created Coulomb prior (#159)
    * Created Coulomb prior
    
    * Bug fix
    
    * Fixed exception when a sample has no interactions within the cutoff
    
    * Fixed error resuming training
    
    * Fixed torchscript errors
    
    * Fixed hardcoded neighborlist size

--- torchmdnet/priors/__init__.py
+++ torchmdnet/priors/__init__.py
@@ -1,5 +1,6 @@
 from torchmdnet.priors.atomref import Atomref
 from torchmdnet.priors.d2 import D2
 from torchmdnet.priors.zbl import ZBL
+from torchmdnet.priors.coulomb import Coulomb
 
-__all__ = ["Atomref", "D2", "ZBL"]
+__all__ = ["Atomref", "D2", "ZBL", "Coulomb"]
