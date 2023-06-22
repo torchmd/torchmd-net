f06f32d9e14cb10fbbcf7c67c8be2dd84eba52d5
Guillem Simeon <55756547+guillemsimeon@users.noreply.github.com>
2023-06-22 11:27:51 +0200

    Update __init__.py for MD22 compatibility

--- torchmdnet/datasets/__init__.py
+++ torchmdnet/datasets/__init__.py
@@ -4,6 +4,7 @@ from .comp6 import ANIMD, DrugBank, GDB07to09, GDB10to13, Tripeptides, S66X8, CO
 from .custom import Custom
 from .hdf import HDF5
 from .md17 import MD17
+from .md22 import MD22
 from .qm9 import QM9
 from .qm9q import QM9q
 from .spice import SPICE
@@ -21,6 +22,7 @@ __all__ = [
     "GDB10to13",
     "HDF5",
     "MD17",
+    "MD22",
     "QM9",
     "QM9q",
     "S66X8",
