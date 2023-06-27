f95353296381a4420fdedaf24eb61e64c6853fe1
RaulPPealez <raulppelaez@gmail.com>
2023-06-27 17:30:21 +0200

    Update inference benchmark

--- benchmarks/inference.py
+++ benchmarks/inference.py
@@ -58,24 +58,14 @@ def benchmark_pdb(pdb_file, **kwargs):
         molecule.coords[:, :, 0], dtype=torch.float32, device=device
     ).to(device)
     molecule = None
-    #print("Number of atoms: %d" % len(atomic_numbers))
     torch.cuda.nvtx.range_push("Initialization")
-    # args = load_example_args(
-    #     "equivariant-transformer",
-    #     config_file="../examples/ET-MD17.yaml",
-    #     remove_prior=True,
-    #     output_model="Scalar",
-    #     derivative=False,
-    #     max_z = int(atomic_numbers.max()+1),
-    #     max_num_neighbors=32,
-    # )
     args = load_example_args(
         "tensornet",
         config_file="../examples/TensorNet-rMD17.yaml",
         remove_prior=True,
         output_model="Scalar",
         derivative=False,
-        max_z = int(atomic_numbers.max()+1),
+        max_z=int(atomic_numbers.max() + 1),
         max_num_neighbors=32,
         **kwargs,
     )
@@ -85,10 +75,7 @@ def benchmark_pdb(pdb_file, **kwargs):
     batch = torch.zeros_like(z).to("cuda")
     model = model.to("cuda")
     torch.cuda.nvtx.range_pop()
-    # Warmup
     torch.cuda.nvtx.range_push("Warmup")
-    #print("Warmup")
-    # Count time
     for i in range(3):
         pred, _ = model(z, pos, batch)
         pred.sum().backward()
@@ -99,7 +86,6 @@ def benchmark_pdb(pdb_file, **kwargs):
         pred.sum().backward()
     torch.cuda.nvtx.range_pop()
     torch.cuda.nvtx.range_push("Benchmark")
-    #print("Benchmark")
     nbench = 100
     times = np.zeros(nbench)
     stream = torch.cuda.Stream()
@@ -107,26 +93,30 @@ def benchmark_pdb(pdb_file, **kwargs):
     with GpuTimer() as timer:
         with torch.cuda.stream(stream):
             for i in range(nbench):
-                #torch.cuda.synchronize()
-                #with GpuTimer() as timer2:
-                    #torch.cuda.nvtx.range_push("Step")
+                # torch.cuda.synchronize()
+                # with GpuTimer() as timer2:
+                # torch.cuda.nvtx.range_push("Step")
                 pred, _ = model(z, pos, batch)
-                    #torch.cuda.nvtx.range_push("derivative")
+                # torch.cuda.nvtx.range_push("derivative")
                 pred.sum().backward()
-                    #torch.cuda.nvtx.range_pop()
-                    #torch.cuda.nvtx.range_pop()
-                    #torch.cuda.synchronize()
-                #times[i] = timer2.interval
+                # torch.cuda.nvtx.range_pop()
+                # torch.cuda.nvtx.range_pop()
+                # torch.cuda.synchronize()
+                # times[i] = timer2.interval
         torch.cuda.synchronize()
-    #torch.cuda.nvtx.range_pop()
-
-    # print(
-    #     "Pdb file: %s (%d atoms) Time: %f ms, stddev %f"
-    #     % (pdb_file, len(atomic_numbers), timer.interval / nbench, times.std())
-    # )
+    # torch.cuda.nvtx.range_pop()
     return len(atomic_numbers), timer.interval / nbench
 
+
 from tabulate import tabulate
+
+# List of cases to benchmark, arbitrary parameters can be overriden here
+cases = {
+    "0L": {"num_layers": 0, "embedding_dimension": 128},
+    "1L": {"num_layers": 1, "embedding_dimension": 128},
+    "2L": {"num_layers": 2, "embedding_dimension": 128},
+    "2L emb 64": {"num_layers": 2, "embedding_dimension": 64},
+}
 
 def benchmark_all():
     timings = {}
@@ -139,34 +129,38 @@ def benchmark_all():
     for pdb_file in os.listdir("systems"):
         if not pdb_file.endswith(".pdb"):
             continue
-        if pdb_file == "stmv.pdb":
+        if pdb_file == "stmv.pdb":  # Does not fit in a 4090
             continue
         times = {}
         num_atoms = 0
-        for nlayers in range(0,3):
-            num_atoms, time = benchmark_pdb(os.path.join("systems", pdb_file), num_layers=nlayers)
-            times[f"%dL"%nlayers] = time
-        num_atoms, time = benchmark_pdb(os.path.join("systems", pdb_file), num_layers=2, embedding_dimension=64)
-        times["2L emb 64"] = time
+        for name, kwargs in cases.items():
+            num_atoms, time = benchmark_pdb(os.path.join("systems", pdb_file), **kwargs)
+            times[name] = time
         timings[pdb_file] = (num_atoms, times)
-    #Print a table with the timings
+    # Print a table with the timings
     keys = list(timings.keys())
     values = list(timings.values())
     labels = [f"Time %s (ms)" % key for key in values[0][1].keys()]
-    table_data = [['Molecule (atoms)', *labels]]
+    table_data = [["Molecule (atoms)", *labels]]
     for key, val in timings.items():
-        #Remove the .pdb extension
+        # Remove the .pdb extension
         key = key[:-4]
-        molecule = '{} ({})'.format(key, val[0])
+        molecule = "{} ({})".format(key, val[0])
         times = []
         for time in val[1].values():
-            times.append(round(time,2))
+            times.append(round(time, 2))
         table_data.append([molecule, *times])
-
-    # Print the table (transposed)
-    table = tabulate(table_data, headers='firstrow', tablefmt='pretty', showindex=False, stralign='center', numalign='center', colalign=("center",))
-    # Print the table
+    table = tabulate(
+        table_data,
+        headers="firstrow",
+        tablefmt="pretty",
+        showindex=False,
+        stralign="center",
+        numalign="center",
+        colalign=("center",),
+    )
     print(table)
+
 
 if __name__ == "__main__":
     benchmark_all()
