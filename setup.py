eeaa20f576516cc25bc41720c8db69a543c39874
Raul <raulppelaez@gmail.com>
2023-09-20 14:35:19 +0200

    Compile torch extension in setup.py (#216)
    * Compile torch extension in setup.py
    
    * Fix extension library discovery
    
    * Make CI more verbose
    
    * Install gxx from conda-forge to ensure ABI compatibility
    
    * Change extension name so it does not collide with NNPOps

--- setup.py
+++ setup.py
@@ -11,11 +11,33 @@ except:
     print("Failed to retrieve the current version, defaulting to 0")
     version = "0"
 
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension, include_paths, CppExtension
+import torch
+
+neighs = CppExtension(
+    name='torchmdnet.neighbors.torchmdnet_neighbors',
+    sources=["torchmdnet/neighbors/neighbors.cpp", "torchmdnet/neighbors/neighbors_cpu.cpp"],
+    include_dirs=include_paths(),
+    language='c++')
+
+if torch.cuda._is_compiled():
+    neighs = CUDAExtension(
+        name='torchmdnet.neighbors.torchmdnet_neighbors',
+        sources=["torchmdnet/neighbors/neighbors.cpp", "torchmdnet/neighbors/neighbors_cpu.cpp", "torchmdnet/neighbors/neighbors_cuda.cu"],
+        include_dirs=include_paths(),
+        language='cuda'
+        )
+
 setup(
     name="torchmd-net",
     version=version,
     packages=find_packages(),
-    package_data={"torchmdnet": ["neighbors/neighbors*", "neighbors/*.cu*"]},
+    ext_modules=[neighs,],
+    cmdclass={
+        'build_ext': BuildExtension.with_options(no_python_abi_suffix=True, use_ninja=False)},
     include_package_data=True,
     entry_points={"console_scripts": ["torchmd-train = torchmdnet.scripts.train:main"]},
+    package_data={"torchmdnet": ["neighbors/torchmdnet_neighbors.so"]},
+
 )
