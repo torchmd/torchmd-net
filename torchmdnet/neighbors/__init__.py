77f9be1295ddc6d8bd136b6883d4c15c1bf4f748
RaulPPealez <raulppelaez@gmail.com>
2023-09-20 12:17:25 +0200

    Compile torch extension in setup.py

--- torchmdnet/neighbors/__init__.py
+++ torchmdnet/neighbors/__init__.py
@@ -1,16 +1,3 @@
-import os
 import torch
-from torch.utils import cpp_extension
-
-def compile_extension():
-    src_dir = os.path.dirname(__file__)
-    sources = ["neighbors.cpp", "neighbors_cpu.cpp"] + (
-        ["neighbors_cuda.cu"] if torch.cuda.is_available() else []
-    )
-    sources = [os.path.join(src_dir, name) for name in sources]
-    cpp_extension.load(
-        name="torchmdnet_neighbors", sources=sources, is_python_module=False
-    )
-
-compile_extension()
-get_neighbor_pairs_kernel = torch.ops.torchmdnet_neighbors.get_neighbor_pairs
+torch.ops.load_library("neighbors.so")
+get_neighbor_pairs_kernel = torch.ops.neighbors.get_neighbor_pairs
