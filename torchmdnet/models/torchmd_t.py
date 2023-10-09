5e5ed2c16ac57c3e423ac36b9cccc62a7051af5c
RaulPPealez <raulppelaez@gmail.com>
2023-10-09 14:57:23 +0200

    Remove dependency on torch_cluster

--- torchmdnet/models/torchmd_t.py
+++ torchmdnet/models/torchmd_t.py
@@ -5,9 +5,10 @@ from torch_geometric.nn import MessagePassing
 from torchmdnet.models.utils import (
     NeighborEmbedding,
     CosineCutoff,
-    Distance,
+    OptimizedDistance,
     rbf_class_mapping,
     act_class_mapping,
+    scatter
 )
 
 
@@ -99,8 +100,8 @@ class TorchMD_T(nn.Module):
 
         self.embedding = nn.Embedding(self.max_z, hidden_channels, dtype=dtype)
 
-        self.distance = Distance(
-            cutoff_lower, cutoff_upper, max_num_neighbors=max_num_neighbors, loop=True
+        self.distance = OptimizedDistance(
+            cutoff_lower, cutoff_upper, max_num_pairs=-max_num_neighbors, loop=True
         )
         self.distance_expansion = rbf_class_mapping[rbf_type](
             cutoff_lower, cutoff_upper, num_rbf, trainable_rbf, dtype=dtype
