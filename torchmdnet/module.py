377fb08e9cbbcad445a2a10ad9dcad80bd464864
RaulPPealez <raulppelaez@gmail.com>
2023-10-11 12:00:01 +0200

    Fix trying to detach a float number

--- torchmdnet/module.py
+++ torchmdnet/module.py
@@ -97,7 +97,9 @@ class LNNP(LightningModule):
         # Returns:
         #   loss_y: loss for the predicted value
         #   loss_neg_y: loss for the predicted negative derivative
-        loss_y, loss_neg_y = 0.0, 0.0
+        loss_y, loss_neg_y = torch.tensor(0.0, device=self.device), torch.tensor(
+            0.0, device=self.device
+        )
         loss_name = loss_fn.__name__
         if self.hparams.derivative and "neg_dy" in batch:
             loss_neg_y = loss_fn(neg_y, batch.neg_dy)
