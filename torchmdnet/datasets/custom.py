90ae6c2fc7dc90064dd43625ae8d72df75066a55
RaulPPealez <raulppelaez@gmail.com>
2023-10-14 18:41:54 +0200

    Improve Custom Dataset performance. Precompute as much as possible by storing mmap'ed files.

--- torchmdnet/datasets/custom.py
+++ torchmdnet/datasets/custom.py
@@ -8,32 +8,28 @@ import h5py
 __all__ = ["Custom"]
 
 
-def write_as_hdf5(coordfiles, embedfiles, energyfiles, forcefiles, hdf5_dataset):
+def write_as_hdf5(files, hdf5_dataset):
     """Transform the input to hdf5 format compatible with the HDF5 Dataset class.
     The input files to the Custom dataset are transformed to a single HDF5 file.
     Args:
-        coordfiles (list): List of coordinate files.
-        embedfiles (list): List of embedding files.
-        energyfiles (list): List of energy files.
-        forcefiles (list): List of force files.
+        files (dict): Dictionary of input files. Must contain "pos", "z" and at least one of "y" or "neg_dy".
         hdf5_dataset (string): Path to the output HDF5 dataset.
     """
     with h5py.File(hdf5_dataset, "w") as f:
-        for i in range(len(coordfiles)):
+        for i in range(len(files["pos"])):
             # Create a group for each file
-            coord_data = np.load(coordfiles[i])
-            embed_data = np.load(embedfiles[i]).astype(int)
+            coord_data = np.load(files["pos"][i])
+            embed_data = np.load(files["z"][i]).astype(int)
             group = f.create_group(str(i))
             num_samples = coord_data.shape[0]
             group["pos"] = coord_data
             group["types"] = np.tile(embed_data, (num_samples, 1))
-            if energyfiles is not None:
-                energy_data = np.load(energyfiles[i])
+            if "y" in files:
+                energy_data = np.load(files["y"][i])
                 group["energy"] = energy_data
-            if forcefiles is not None:
-                force_data = np.load(forcefiles[i])
+            if "neg_dy" in files:
+                force_data = np.load(files["neg_dy"][i])
                 group["forces"] = force_data
-
 
 
 class Custom(Dataset):
@@ -79,39 +75,37 @@ class Custom(Dataset):
         )
         self.has_energies = energyglob is not None
         self.has_forces = forceglob is not None
-
-        self.coordfiles = sorted(glob.glob(coordglob))
-        self.embedfiles = sorted(glob.glob(embedglob))
-        self.energyfiles = sorted(glob.glob(energyglob)) if self.has_energies else None
-        self.forcefiles = sorted(glob.glob(forceglob)) if self.has_forces else None
-
-        assert len(self.coordfiles) == len(self.embedfiles), (
-            f"Number of coordinate files {len(self.coordfiles)} "
-            f"does not match number of embed files {len(self.embedfiles)}."
+        self.fields = [
+            ("pos", "pos", torch.float32),
+            ("z", "types", torch.long),
+        ]
+        self.files = {}
+        self.files["pos"] = sorted(glob.glob(coordglob))
+        self.files["z"] = sorted(glob.glob(embedglob))
+        assert len(self.files["pos"]) == len(self.files["z"]), (
+            f"Number of coordinate files {len(self.files['pos'])} "
+            f"does not match number of embed files {len(self.files['z'])}."
         )
         if self.has_energies:
-            assert len(self.coordfiles) == len(self.energyfiles), (
-                f"Number of coordinate files {len(self.coordfiles)} "
-                f"does not match number of energy files {len(self.energyfiles)}."
+            self.files["y"] = sorted(glob.glob(energyglob))
+            self.fields.append(("y", "energy", torch.float32))
+            assert len(self.files["pos"]) == len(self.files["y"]), (
+                f"Number of coordinate files {len(self.files['pos'])} "
+                f"does not match number of energy files {len(self.files['y'])}."
             )
         if self.has_forces:
-            assert len(self.coordfiles) == len(self.forcefiles), (
-                f"Number of coordinate files {len(self.coordfiles)} "
-                f"does not match number of force files {len(self.forcefiles)}."
+            self.files["neg_dy"] = sorted(glob.glob(forceglob))
+            self.fields.append(("neg_dy", "forces", torch.float32))
+            assert len(self.files["pos"]) == len(self.files["neg_dy"]), (
+                f"Number of coordinate files {len(self.files['pos'])} "
+                f"does not match number of force files {len(self.files['neg_dy'])}."
             )
-        print("Number of files: ", len(self.coordfiles))
-        self.load_into_memory = False
+        print("Number of files: ", len(self.files["pos"]))
+        self.cached = False
         if read_as_hdf5 is not None:
             hdf5_file = read_as_hdf5
-            if not self.has_energies:
-                raise ValueError(
-                    "HDF5 format requires energies to be present in the dataset."
-                )
             write_as_hdf5(
-                self.coordfiles,
-                self.embedfiles,
-                self.energyfiles,
-                self.forcefiles,
+                self.files,
                 hdf5_file,
             )
             self.hdf5_dataset = HDF5(hdf5_file)
@@ -119,57 +113,90 @@ class Custom(Dataset):
         print("Combined dataset size {}".format(len(self.index)))
         # If the dataset is small enough, load it whole into CPU memory
         data_size_limit = preload_memory_limit * 1024 * 1024
+        self.cache = False
         if total_data_size < data_size_limit:
-            self.load_into_memory = True
+            self.cached = True
             print(
-                "Preloading Custom dataset (of size {:.2f} MB) into CPU memory".format(
-                    total_data_size / 1024 / 1024
+                "Preloading Custom dataset (of size {:.2f} MB)".format(
+                    total_data_size / 1024**2
                 )
             )
-            self._store_as_tensors()
+            self._preload_data()
+        else:
+            self._store_numpy_memmaps()
 
-    def _store_as_tensors(self):
+    def _preload_data(self):
         """Load the input files as Torch tensors.
+        Each file can have different number of atoms, so each one is stored in a different tensor.
         """
-        self.coordfiles = torch.from_numpy(np.array([np.load(f) for f in self.coordfiles]))
-        self.embedfiles = torch.from_numpy(
-            np.array([np.load(f).astype(int) for f in self.embedfiles])
-        )
+        self.stored_data = {}
+        self.stored_data["pos"] = [
+            torch.from_numpy(np.load(f)) for f in self.files["pos"]
+        ]
+        self.stored_data["z"] = [
+            torch.from_numpy(np.load(f).astype(int))
+            .unsqueeze(0)
+            .expand(self.stored_data["pos"][i].shape[0], -1)
+            for i, f in enumerate(self.files["z"])
+        ]
         if self.has_energies:
-            self.energyfiles = torch.from_numpy(np.array([np.load(f) for f in self.energyfiles]))
+            self.stored_data["y"] = [
+                torch.from_numpy(np.load(f)) for f in self.files["y"]
+            ]
         if self.has_forces:
-            self.forcefiles = torch.from_numpy(np.array([np.load(f) for f in self.forcefiles]))
+            self.stored_data["neg_dy"] = [
+                torch.from_numpy(np.load(f)) for f in self.files["neg_dy"]
+            ]
+
+    def _store_numpy_memmaps(self):
+        """Create a dictionary with numpy arrays with mmap_mode="r" for each file."""
+        self.stored_data = {}
+        self.stored_data["pos"] = [np.load(f, mmap_mode="r") for f in self.files["pos"]]
+        self.stored_data["z"] = []
+        for i, f in enumerate(self.files["z"]):
+            loaded_data = np.load(f).astype(int)
+            desired_shape = (len(self.stored_data["pos"][i]), loaded_data.shape[0])
+            broadcasted_data = np.broadcast_to(
+                loaded_data[np.newaxis, :], desired_shape
+            )
+            self.stored_data["z"].append(broadcasted_data)
+        if self.has_energies:
+            self.stored_data["y"] = [np.load(f, mmap_mode="r") for f in self.files["y"]]
+        if self.has_forces:
+            self.stored_data["neg_dy"] = [
+                np.load(f, mmap_mode="r") for f in self.files["neg_dy"]
+            ]
 
     def _initialize_index(self):
         """Initialize the index for the dataset.
+        The index relates a sample to the file it belongs to and the index within that file.
         Returns:
             int: Total size of the dataset in bytes.
         """
         # create index
         self.index = []
-        nfiles = len(self.coordfiles)
+        nfiles = len(self.files["pos"])
         total_data_size = 0  # Number of bytes in the dataset
         for i in range(nfiles):
-            coord_data = np.load(self.coordfiles[i])
-            embed_data = np.load(self.embedfiles[i]).astype(int)
+            coord_data = np.load(self.files["pos"][i], mmap_mode="r")
+            embed_data = np.load(self.files["z"][i]).astype(int)
             size = coord_data.shape[0]
             total_data_size += coord_data.nbytes + embed_data.nbytes
             self.index.extend(list(zip([i] * size, range(size))))
-
             # consistency check
             assert coord_data.shape[1] == embed_data.shape[0], (
                 f"Number of atoms in coordinate file {i} ({coord_data.shape[1]}) "
                 f"does not match number of atoms in embed file {i} ({embed_data.shape[0]})."
             )
             if self.has_energies:
-                energy_data = np.load(self.energyfiles[i])
+                energy_data = np.load(self.files["y"][i], mmap_mode="r")
                 total_data_size += energy_data.nbytes
                 assert coord_data.shape[0] == energy_data.shape[0], (
                     f"Number of frames in coordinate file {i} ({coord_data.shape[0]}) "
                     f"does not match number of frames in energy file {i} ({energy_data.shape[0]})."
                 )
             if self.has_forces:
-                force_data = np.load(self.forcefiles[i])
+                force_data = np.load(self.files["neg_dy"][i], mmap_mode="r")
                 total_data_size += force_data.nbytes
                 assert coord_data.shape == force_data.shape, (
                     f"Data shape of coordinate file {i} {coord_data.shape} "
@@ -181,37 +208,11 @@ class Custom(Dataset):
         if hasattr(self, "hdf5_dataset"):
             return self.hdf5_dataset.get(idx)
         fileid, index = self.index[idx]
-        if self.load_into_memory:
-            coord_data = self.coordfiles[fileid][index]
-            embed_data = self.embedfiles[fileid]
-        else:
-            coord_data = torch.from_numpy(
-                np.array(np.load(self.coordfiles[fileid], mmap_mode="r")[index])
-            )
-            embed_data = torch.from_numpy(np.load(self.embedfiles[fileid]).astype(int))
-
-        features = dict(pos=coord_data, z=embed_data)
-
-        if self.has_energies and self.load_into_memory:
-            energy_data = self.energyfiles[fileid][index]
-            features["y"] = energy_data
-        elif self.has_energies:
-            energy_data = torch.from_numpy(
-                np.array(np.load(self.energyfiles[fileid], mmap_mode="r")[index])
-            )
-
-            features["y"] = energy_data
-
-        if self.has_forces and self.load_into_memory:
-            force_data = self.forcefiles[fileid][index]
-            features["neg_dy"] = force_data
-        elif self.has_forces:
-            force_data = torch.from_numpy(
-                np.array(np.load(self.forcefiles[fileid], mmap_mode="r")[index])
-            )
-            features["neg_dy"] = force_data
-
-        return Data(**features)
+        data = Data()
+        for field in self.fields:
+            f = self.stored_data[field[0]][fileid][index]
+            data[field[0]] = f if self.cached else torch.from_numpy(np.array(f))
+        return data
 
     def len(self):
         return len(self.index)
