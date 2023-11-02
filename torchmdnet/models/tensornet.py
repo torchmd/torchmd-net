f832d2b71e6947ffae089cd908bb6a2dd0ec964e
Guillem Simeon <55756547+guillemsimeon@users.noreply.github.com>
2023-11-02 13:11:33 +0100

    Update tensornet.py for q support

--- torchmdnet/models/tensornet.py
+++ torchmdnet/models/tensornet.py
@@ -217,6 +217,12 @@ class TensorNet(nn.Module):
             mask = (edge_index[0] < 0).unsqueeze(0).expand_as(edge_index)
             zp = torch.cat((z, torch.zeros(1, device=z.device, dtype=z.dtype)), dim=0)
             # I trick the model into thinking that the masked edges pertain to the extra atom
+            if q is None:
+                q = 0
+            else:
+                batchp = torch.cat((batch, batch[-1] + 1), dim=0)
+                qp = torch.cat((q, torch.zeros(1, device=q.device, dtype=q.dtype)), dim=0)
+                q = qp[batchp][...,None,None,None]
             # WARNING: This can hurt performance if max_num_pairs >> actual_num_pairs
             edge_index = edge_index.masked_fill(mask, z.shape[0])
             edge_weight = edge_weight.masked_fill(mask[0], 0)
@@ -230,7 +236,7 @@ class TensorNet(nn.Module):
         if q is None:
             q = 0
         else:
-            q = q[...,None,None,None]
+            q = q[batch][...,None,None,None]
         for layer in self.layers:
             X = layer(X, edge_index, edge_weight, edge_attr, q)
         I, A, S = decompose_tensor(X)
