d006ca118b70c2e0c3731b531e2c35e1f6c8694f
RaulPPealez <raulppelaez@gmail.com>
2023-12-14 14:09:11 +0100

    Add copyright notice to every python script

--- torchmdnet/models/model.py
+++ torchmdnet/models/model.py
@@ -1,3 +1,7 @@
+# Copyright Universitat Pompeu Fabra 2020-2023  https://www.compscience.org
+# Distributed under the MIT License.
+# (See accompanying file README.md file or copy at http://opensource.org/licenses/MIT)
+
 import re
 from typing import Optional, List, Tuple, Dict
 import torch
