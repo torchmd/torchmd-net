f22bcb88dcb2b42b4b95040a7d66cbe33ca8ae25
RaulPPealez <raulppelaez@gmail.com>
2023-12-14 14:09:11 +0100

    Add copyright notice to every python script

--- tests/test_dataset_comp6.py
+++ tests/test_dataset_comp6.py
@@ -1,3 +1,7 @@
+# Copyright Universitat Pompeu Fabra 2020-2023  https://www.compscience.org
+# Distributed under the MIT License.
+# (See accompanying file README.md file or copy at http://opensource.org/licenses/MIT)
+
 import torch as pt
 from tempfile import TemporaryDirectory
 from torch_geometric.loader import DataLoader
--- tests/test_rbfs.py
+++ tests/test_rbfs.py
@@ -1,3 +1,7 @@
+# Copyright Universitat Pompeu Fabra 2020-2023  https://www.compscience.org
+# Distributed under the MIT License.
+# (See accompanying file README.md file or copy at http://opensource.org/licenses/MIT)
+
 from pytest import mark
 import torch
 from torchmdnet.models.utils import rbf_class_mapping
