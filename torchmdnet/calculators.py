793cb5aabb6f87118383983c08e4f6b82ac8b4d3
RaulPPealez <raulppelaez@gmail.com>
2024-02-16 16:49:48 +0100

    typo

--- torchmdnet/calculators.py
+++ torchmdnet/calculators.py
@@ -129,7 +129,7 @@ class External:
         """
         pos = pos.to(self.device).to(self.dtype).reshape(-1, 3)
         if box is not None:
-            box = box.to(self.device).to(self.dtype).reshape(-1, 3, 3)
+            box = box.to(self.device).to(self.dtype).reshape(-1, 3)
         if self.use_cuda_graph:
             if self.pos is None:
                 self.pos = (
