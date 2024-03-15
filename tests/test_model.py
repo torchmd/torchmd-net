3cb232e721eefae000c71d1ba27d9868eb404b29
Antonio Mirarchi <antonio.mirarchi@upf.edu>
2024-03-15 09:49:42 +0100

    add test for additional labels

--- tests/test_model.py
+++ tests/test_model.py
@@ -18,22 +18,24 @@ from utils import load_example_args, create_example_batch
 
 @mark.parametrize("model_name", models.__all_models__)
 @mark.parametrize("use_batch", [True, False])
-@mark.parametrize("explicit_q_s", [True, False])
+@mark.parametrize("use_extra_args", [True, False])
 @mark.parametrize("precision", [32, 64])
-def test_forward(model_name, use_batch, explicit_q_s, precision):
+@mark.parametrize("additional_labels", [None, {"tensornet_q": {"label": "total_charge", 'learnable': False, 'init_value': 0.1}}])
+def test_forward(model_name, use_batch, use_extra_args, precision, additional_labels):
     z, pos, batch = create_example_batch()
     pos = pos.to(dtype=dtype_mapping[precision])
-    model = create_model(load_example_args(model_name, prior_model=None, precision=precision))
+    model = create_model(load_example_args(model_name, prior_model=None, precision=precision, additional_labels=additional_labels))
     batch = batch if use_batch else None
-    if explicit_q_s:
-        model(z, pos, batch=batch, q=None, s=None)
+    if use_extra_args:
+        model(z, pos, batch=batch, extra_args={'total_charge': torch.zeros_like(z)})
     else:
+        print("No Extra Args provided")
         model(z, pos, batch=batch)
 
 
 @mark.parametrize("model_name", models.__all_models__)
 @mark.parametrize("output_model", output_modules.__all__)
-@mark.parametrize("precision", [32,64])
+@mark.parametrize("precision", [32, 64])
 def test_forward_output_modules(model_name, output_model, precision):
     z, pos, batch = create_example_batch()
     args = load_example_args(model_name, remove_prior=True, output_model=output_model, precision=precision)
