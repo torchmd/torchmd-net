72d6e8e9176452d804df42dd39942cef2b82f874
Raul <raulppelaez@gmail.com>
2024-04-15 13:19:28 +0200

    torch.compile neighbors without graph breaks (#305)
    * Add test for full graph neighbor torch.compile
    
    * blacken
    
    * Add missing sqrt default implementation
    
    * Update compile test
    
    * Import torch.Tensor
    
    * Expose only the extensions
    
    * Make CUDA backwards also the operation used in CPU
    Make fwd and bkwd independent operators
    Add meta registrations for forwards and backwards
    Define meta registrations only in pytorch>=2.2.0
    
    * Fix CPU extentension potentially allocating with a negative size
    
    * Fix incorrect type

--- torchmdnet/extensions/neighbors/neighbors_cpu.cpp
+++ torchmdnet/extensions/neighbors/neighbors_cpu.cpp
@@ -1,7 +1,7 @@
 /* Copyright Universitat Pompeu Fabra 2020-2023  https://www.compscience.org
  * Distributed under the MIT License.
  *(See accompanying file README.md file or copy at http://opensource.org/licenses/MIT)
-*/
+ */
 #include <torch/extension.h>
 #include <tuple>
 
@@ -18,19 +18,22 @@ using torch::round;
 using torch::Scalar;
 using torch::Tensor;
 using torch::vstack;
+using torch::autograd::AutogradContext;
+using torch::autograd::Function;
+using torch::autograd::tensor_list;
 using torch::indexing::None;
 using torch::indexing::Slice;
 
 static tuple<Tensor, Tensor, Tensor, Tensor>
-forward(const Tensor& positions, const Tensor& batch, const Tensor& in_box_vectors,
-        bool use_periodic, const Scalar& cutoff_lower, const Scalar& cutoff_upper,
-        const Scalar& max_num_pairs, bool loop, bool include_transpose) {
+forward_impl(const std::string& strategy, const Tensor& positions, const Tensor& batch,
+             const Tensor& in_box_vectors, bool use_periodic, const Scalar& cutoff_lower,
+             const Scalar& cutoff_upper, const Scalar& max_num_pairs, bool loop,
+             bool include_transpose) {
     TORCH_CHECK(positions.dim() == 2, "Expected \"positions\" to have two dimensions");
     TORCH_CHECK(positions.size(0) > 0,
                 "Expected the 1nd dimension size of \"positions\" to be more than 0");
     TORCH_CHECK(positions.size(1) == 3, "Expected the 2nd dimension size of \"positions\" to be 3");
     TORCH_CHECK(positions.is_contiguous(), "Expected \"positions\" to be contiguous");
-
     TORCH_CHECK(cutoff_upper.to<double>() > 0, "Expected \"cutoff\" to be positive");
     Tensor box_vectors = in_box_vectors;
     const int n_batch = batch.max().item<int>() + 1;
@@ -44,7 +47,8 @@ forward(const Tensor& positions, const Tensor& batch, const Tensor& in_box_vecto
         // Ensure the box first dimension has size max(batch) + 1
         TORCH_CHECK(box_vectors.size(0) == n_batch,
                     "Expected \"box_vectors\" to have shape (n_batch, 3, 3)");
-	// Check that the box is a valid triclinic box, in the case of a box per sample we only check the first one
+        // Check that the box is a valid triclinic box, in the case of a box per sample we only
+        // check the first one
         double v[3][3];
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
@@ -113,16 +117,123 @@ forward(const Tensor& positions, const Tensor& batch, const Tensor& in_box_vecto
     }
     Tensor num_pairs_found = torch::empty(1, distances.options().dtype(kInt32));
     num_pairs_found[0] = distances.size(0);
+    // This seems wasteful, but it allows to enable torch.compile by guaranteeing that the output of
+    // this operator has a predictable size Resize to max_num_pairs, add zeros if necessary
+    int64_t extension = std::max(max_num_pairs.toLong() - distances.size(0), (int64_t)0);
+    if (extension > 0) {
+        deltas = torch::vstack({deltas, torch::zeros({extension, 3}, deltas.options())});
+        distances = torch::hstack({distances, torch::zeros({extension}, distances.options())});
+        // For the neighbors add (-1,-1) pairs to fill the tensor
+        neighbors = torch::hstack(
+            {neighbors, torch::full({2, extension}, -1, neighbors.options().dtype(kInt32))});
+    }
     return {neighbors, deltas, distances, num_pairs_found};
 }
 
-TORCH_LIBRARY_IMPL(torchmdnet_extensions, CPU, m) {
+// The backwards operation is implemented fully in pytorch so that it can be differentiated a second
+// time automatically via Autograd.
+static Tensor backward_impl(const Tensor& grad_edge_vec, const Tensor& grad_edge_weight,
+                            const Tensor& edge_index, const Tensor& edge_vec,
+                            const Tensor& edge_weight, const int64_t num_atoms) {
+    auto zero_mask = edge_weight.eq(0);
+    auto zero_mask3 = zero_mask.unsqueeze(-1).expand_as(grad_edge_vec);
+    // We need to avoid dividing by 0. Otherwise Autograd fills the gradient with NaNs in the
+    // case of a double backwards. This is why we index_select like this.
+    auto grad_distances_ = edge_vec / edge_weight.masked_fill(zero_mask, 1).unsqueeze(-1) *
+                           grad_edge_weight.masked_fill(zero_mask, 0).unsqueeze(-1);
+    auto result = grad_edge_vec.masked_fill(zero_mask3, 0) + grad_distances_;
+    // Given that there is no masked_index_add function, in order to make the operation
+    // CUDA-graph compatible I need to transform masked indices into a dummy value (num_atoms)
+    // and then exclude that value from the output.
+    // TODO: replace this once masked_index_add  or masked_scatter_add are available
+    auto grad_positions_ = torch::zeros({num_atoms + 1, 3}, edge_vec.options());
+    auto edge_index_ =
+        edge_index.masked_fill(zero_mask.unsqueeze(0).expand_as(edge_index), num_atoms);
+    grad_positions_.index_add_(0, edge_index_[0], result);
+    grad_positions_.index_add_(0, edge_index_[1], -result);
+    auto grad_positions = grad_positions_.index({Slice(0, num_atoms), Slice()});
+    return grad_positions;
+}
+
+// This is the autograd function that is called when the user calls get_neighbor_pairs.
+// It dispatches the required strategy for the forward function and implements the backward
+// function. The backward function is written in full pytorch so that it can be differentiated a
+// second time automatically via Autograd.
+class NeighborAutograd : public torch::autograd::Function<NeighborAutograd> {
+public:
+    static tensor_list forward(AutogradContext* ctx, const std::string& strategy,
+                               const Tensor& positions, const Tensor& batch,
+                               const Tensor& box_vectors, bool use_periodic,
+                               const Scalar& cutoff_lower, const Scalar& cutoff_upper,
+                               const Scalar& max_num_pairs, bool loop, bool include_transpose) {
+        static auto fwd =
+            torch::Dispatcher::singleton()
+                .findSchemaOrThrow("torchmdnet_extensions::get_neighbor_pairs_fwd", "")
+                .typed<decltype(forward_impl)>();
+        Tensor neighbors, deltas, distances, i_curr_pair;
+        std::tie(neighbors, deltas, distances, i_curr_pair) =
+            fwd.call(strategy, positions, batch, box_vectors, use_periodic, cutoff_lower,
+                     cutoff_upper, max_num_pairs, loop, include_transpose);
+        ctx->save_for_backward({neighbors, deltas, distances});
+        ctx->saved_data["num_atoms"] = positions.size(0);
+        return {neighbors, deltas, distances, i_curr_pair};
+    }
+
+    using Slice = torch::indexing::Slice;
+
+    static tensor_list backward(AutogradContext* ctx, tensor_list grad_outputs) {
+        auto saved = ctx->get_saved_variables();
+        auto edge_index = saved[0];
+        auto edge_vec = saved[1];
+        auto edge_weight = saved[2];
+        auto num_atoms = ctx->saved_data["num_atoms"].toInt();
+        auto grad_edge_vec = grad_outputs[1];
+        auto grad_edge_weight = grad_outputs[2];
+        static auto backward =
+            torch::Dispatcher::singleton()
+                .findSchemaOrThrow("torchmdnet_extensions::get_neighbor_pairs_bkwd", "")
+                .typed<decltype(backward_impl)>();
+        auto grad_positions = backward.call(grad_edge_vec, grad_edge_weight, edge_index, edge_vec,
+                                            edge_weight, num_atoms);
+        Tensor ignore;
+        return {ignore, grad_positions, ignore, ignore, ignore, ignore,
+                ignore, ignore,         ignore, ignore, ignore};
+    }
+};
+
+// By registering as a CompositeImplicitAutograd we can torch.compile this Autograd function.
+// This mode will generate meta registration for NeighborAutograd automatically, in this case using
+// the
+//  meta registrations provided for the forward and backward functions python side.
+// We provide meta registrations python side because it is the recommended way to do it.
+TORCH_LIBRARY_IMPL(torchmdnet_extensions, CompositeImplicitAutograd, m) {
     m.impl("get_neighbor_pairs",
            [](const std::string& strategy, const Tensor& positions, const Tensor& batch,
               const Tensor& box_vectors, bool use_periodic, const Scalar& cutoff_lower,
               const Scalar& cutoff_upper, const Scalar& max_num_pairs, bool loop,
               bool include_transpose) {
-               return forward(positions, batch, box_vectors, use_periodic, cutoff_lower,
-                              cutoff_upper, max_num_pairs, loop, include_transpose);
+               auto result = NeighborAutograd::apply(strategy, positions, batch, box_vectors,
+                                                     use_periodic, cutoff_lower, cutoff_upper,
+                                                     max_num_pairs, loop, include_transpose);
+               return std::make_tuple(result[0], result[1], result[2], result[3]);
            });
+}
+
+// The registration for the CUDA version of the forward function is done in a separate .cu file.
+TORCH_LIBRARY_IMPL(torchmdnet_extensions, CPU, m) {
+    m.impl("get_neighbor_pairs_fwd", forward_impl);
+}
+
+// Ideally we would register this just once using CompositeImplicitAutograd, but this causes a
+// segfault
+//  when trying to torch.compile this function.
+// Doing it this way prints a message about Autograd not being provided a backward function of the
+// backward function. It gets it from the implementation just fine now, but warns that in the future
+// this will be deprecated.
+TORCH_LIBRARY_IMPL(torchmdnet_extensions, CPU, m) {
+    m.impl("get_neighbor_pairs_bkwd", backward_impl);
+}
+
+TORCH_LIBRARY_IMPL(torchmdnet_extensions, CUDA, m) {
+    m.impl("get_neighbor_pairs_bkwd", backward_impl);
 }
--- torchmdnet/extensions/neighbors/neighbors_cuda.cu
+++ torchmdnet/extensions/neighbors/neighbors_cuda.cu
@@ -8,84 +8,30 @@
 #include "neighbors_cuda_cell.cuh"
 #include "neighbors_cuda_shared.cuh"
 #include <torch/extension.h>
-template <class... T> auto call_forward_kernel(const std::string& kernel_name, const T&... args) {
-    if (kernel_name == "brute") {
-        return forward_brute(args...);
-    } else if (kernel_name == "cell") {
-        return forward_cell(args...);
-    } else if (kernel_name == "shared") {
-        return forward_shared(args...);
+static std::tuple<Tensor, Tensor, Tensor, Tensor>
+forward_impl_cuda(const std::string& strategy, const Tensor& positions, const Tensor& batch,
+                  const Tensor& in_box_vectors, bool use_periodic, const Scalar& cutoff_lower,
+                  const Scalar& cutoff_upper, const Scalar& max_num_pairs, bool loop,
+                  bool include_transpose) {
+    auto kernel = forward_brute;
+    if (positions.size(0) >= 32768 && strategy == "brute") {
+        kernel = forward_shared;
+    }
+    if (strategy == "brute") {
+    } else if (strategy == "cell") {
+        kernel = forward_cell;
+    } else if (strategy == "shared") {
+        kernel = forward_shared;
     } else {
         throw std::runtime_error("Unknown kernel name");
     }
+    return kernel(positions, batch, in_box_vectors, use_periodic, cutoff_lower, cutoff_upper,
+                  max_num_pairs, loop, include_transpose);
 }
 
-// This is the autograd function that is called when the user calls get_neighbor_pairs.
-// It dispatches the required strategy for the forward function and implements the backward
-// function. The backward function is written in full pytorch so that it can be differentiated a
-// second time automatically via Autograd.
-class NeighborAutograd : public torch::autograd::Function<NeighborAutograd> {
-public:
-    static tensor_list forward(AutogradContext* ctx, const std::string& strategy,
-                               const Tensor& positions, const Tensor& batch,
-                               const Tensor& box_vectors, bool use_periodic,
-                               const Scalar& cutoff_lower, const Scalar& cutoff_upper,
-                               const Scalar& max_num_pairs, bool loop, bool include_transpose) {
-        Tensor neighbors, deltas, distances, i_curr_pair;
-        std::tie(neighbors, deltas, distances, i_curr_pair) =
-            call_forward_kernel(strategy, positions, batch, box_vectors, use_periodic, cutoff_lower,
-                                cutoff_upper, max_num_pairs, loop, include_transpose);
-        ctx->save_for_backward({neighbors, deltas, distances});
-        ctx->saved_data["num_atoms"] = positions.size(0);
-        return {neighbors, deltas, distances, i_curr_pair};
-    }
-
-    using Slice = torch::indexing::Slice;
-
-    static tensor_list backward(AutogradContext* ctx, tensor_list grad_outputs) {
-        auto saved = ctx->get_saved_variables();
-        auto edge_index = saved[0];
-        auto edge_vec = saved[1];
-        auto edge_weight = saved[2];
-        auto num_atoms = ctx->saved_data["num_atoms"].toInt();
-        auto grad_edge_vec = grad_outputs[1];
-        auto grad_edge_weight = grad_outputs[2];
-        auto zero_mask = edge_weight == 0;
-        auto zero_mask3 = zero_mask.unsqueeze(-1).expand_as(grad_edge_vec);
-        // We need to avoid dividing by 0. Otherwise Autograd fills the gradient with NaNs in the
-        // case of a double backwards. This is why we index_select like this.
-        auto grad_distances_ = edge_vec / edge_weight.masked_fill(zero_mask, 1).unsqueeze(-1) *
-                               grad_edge_weight.masked_fill(zero_mask, 0).unsqueeze(-1);
-        auto result = grad_edge_vec.masked_fill(zero_mask3, 0) + grad_distances_;
-        // Given that there is no masked_index_add function, in order to make the operation
-        // CUDA-graph compatible I need to transform masked indices into a dummy value (num_atoms)
-        // and then exclude that value from the output.
-	// TODO: replace this once masked_index_add  or masked_scatter_add are available
-        auto grad_positions_ = torch::zeros({num_atoms + 1, 3}, edge_vec.options());
-        auto edge_index_ =
-            edge_index.masked_fill(zero_mask.unsqueeze(0).expand_as(edge_index), num_atoms);
-        grad_positions_.index_add_(0, edge_index_[0], result);
-        grad_positions_.index_add_(0, edge_index_[1], -result);
-        auto grad_positions = grad_positions_.index({Slice(0, num_atoms), Slice()});
-        Tensor ignore;
-        return {ignore, grad_positions, ignore, ignore, ignore, ignore,
-                ignore, ignore,         ignore, ignore, ignore};
-    }
-};
-
-TORCH_LIBRARY_IMPL(torchmdnet_extensions, AutogradCUDA, m) {
-    m.impl("get_neighbor_pairs",
-           [](const std::string& strategy, const Tensor& positions, const Tensor& batch,
-              const Tensor& box_vectors, bool use_periodic, const Scalar& cutoff_lower,
-              const Scalar& cutoff_upper, const Scalar& max_num_pairs, bool loop,
-              bool include_transpose) {
-               auto final_strategy = strategy;
-               if (positions.size(0) >= 32768 && strategy == "brute") {
-                   final_strategy = "shared";
-               }
-               auto result = NeighborAutograd::apply(final_strategy, positions, batch, box_vectors,
-                                                     use_periodic, cutoff_lower, cutoff_upper,
-                                                     max_num_pairs, loop, include_transpose);
-               return std::make_tuple(result[0], result[1], result[2], result[3]);
-           });
+// We only need to register the CUDA version of the forward function here. This way we can avoid
+// compiling this file in CPU-only mode The rest of the registrations take place in
+// neighbors_cpu.cpp
+TORCH_LIBRARY_IMPL(torchmdnet_extensions, CUDA, m) {
+    m.impl("get_neighbor_pairs_fwd", forward_impl_cuda);
 }
