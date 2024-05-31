78ef15ac2608663aabe1fbbdb1cb6d6f10977010
Antonio Mirarchi <antonio.mirarchi@upf.edu>
2024-05-31 12:14:58 +0200

    remove unused instance

--- torchmdnet/datasets/mdcath.py
+++ torchmdnet/datasets/mdcath.py
@@ -79,7 +79,6 @@ class mdCATH(Dataset):
         """
 
         self.url = "https://zenodo.org/record/<record_id>/files/"
-        self.preload_dataset_limit = preload_dataset_limit
         super(mdCATH, self).__init__(root, transform, pre_transform, pre_filter)
         self.source_file = "mdcath_source.h5"
         self.numAtoms = numAtoms
