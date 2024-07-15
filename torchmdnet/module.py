dfba2b0662ea2f06d4fcaaa4ecb8afe53a955993
RaulPPealez <raulppelaez@gmail.com>
2024-07-15 12:08:16 +0200

    Change name to ema_parameters_decay, fix initialization

--- torchmdnet/module.py
+++ torchmdnet/module.py
@@ -74,11 +74,16 @@ class LNNP(LightningModule):
         else:
             self.model = create_model(self.hparams, prior_model, mean, std)
 
-        self.ema_prmtrs = None
-        if self.hparams.ema_prmtrs_decay is not None:
+        self.ema_parameters = None
+        if (
+            "ema_parameters_decay" in self.hparams
+            and self.hparams.ema_parameters_decay is not None
+        ):
             # initialize EMA for the model paremeters
-            self.ema_prmtrs = ExponentialMovingAverage(self.model.parameters(), decay=self.hparams.ema_prmtrs_decay)
-        
+            self.ema_parameters = ExponentialMovingAverage(
+                self.model.parameters(), decay=self.hparams.ema_parameters_decay
+            )
+
         # initialize exponential smoothing
         self.ema = None
         self._reset_ema_dict()
@@ -257,11 +262,11 @@ class LNNP(LightningModule):
                 pg["lr"] = lr_scale * self.hparams.lr
         super().optimizer_step(*args, **kwargs)
         optimizer.zero_grad()
-    
+
     def on_before_zero_grad(self, *args, **kwargs):
-        if self.ema_prmtrs is not None:
-            self.ema_prmtrs.to(self.device)
-            self.ema_prmtrs.update(self.model.parameters())
+        if self.ema_parameters is not None:
+            self.ema_parameters.to(self.device)
+            self.ema_parameters.update(self.model.parameters())
 
     def _get_mean_loss_dict_for_type(self, type):
         # Returns a list with the mean loss for each loss_fn for each stage (train, val, test)
