6134086a27169debf51674757479da392ba4411f
Antonio Mirarchi <antonio.mirarchi@upf.edu>
2024-07-19 18:02:40 +0200

    reorder and update code

--- torchmdnet/datasets/mdcath.py
+++ torchmdnet/datasets/mdcath.py
@@ -3,31 +3,30 @@
 # (See accompanying file README.md file or copy at http://opensource.org/licenses/MIT)
 
 import os
-from os.path import join as opj
 import h5py
 import torch
-from tqdm import tqdm
 import math
+import logging
 import numpy as np
-from torch_geometric.data import Dataset, download_url, Data
+from tqdm import tqdm
+from os.path import join as opj
+from torch_geometric.data import Dataset, Data
+import urllib.request
+from collections import defaultdict
 
+logger = logging.getLogger('MDCATH')
 
-def get_pdb_list(pdb_list):
-    # pdb list could be a list of pdb ids or a file with the pdb ids
+def load_pdb_list(pdb_list):
+    """Load PDB list from a file or return list directly."""
     if isinstance(pdb_list, list):
         return pdb_list
-    elif isinstance(pdb_list, str):
-        if os.path.exists(pdb_list):
-            print(f"Reading PDB list from {pdb_list}")
-            with open(pdb_list, "r") as f:
-                return [line.strip() for line in f]
-        else:
-            raise FileNotFoundError(f"File {pdb_list} not found")
-    else:
-        return None
+    elif isinstance(pdb_list, str) and os.path.isfile(pdb_list):
+        logger.info(f"Reading PDB list from {pdb_list}")
+        with open(pdb_list, "r") as file:
+            return [line.strip() for line in file]
+    raise ValueError("Invalid PDB list. Please provide a list or a path to a file.")
 
-
-class mdCATH(Dataset):
+class MDCATH(Dataset):
     def __init__(
         self,
         root,
@@ -35,7 +34,6 @@ class mdCATH(Dataset):
         pre_transform=None,
         pre_filter=None,
         numAtoms=5000,
-        numNoHAtoms=None,
         numResidues=1000,
         temperatures=["348"],
         skipFrames=1,
@@ -52,8 +50,6 @@ class mdCATH(Dataset):
         -----------
         root: str
             Root directory where the dataset should be stored. Data will be downloaded to 'root/'.
-        preload_dataset_limit: int
-            Maximum size of the dataset in MB to load into memory. If the dataset is larger than this limit, a warning will be printed. Default is 1024 MB.
         numAtoms: int
             Max number of atoms in the protein structure.
         numNoHAtoms: int
@@ -65,7 +61,8 @@ class mdCATH(Dataset):
         skipFrames: int
             Number of frames to skip in the trajectory. Default is 1.
         pdb_list: list or str
-            List of PDB IDs to download. If None, all available PDB IDs from 'mdcath_source.h5' will be downloaded.
+            List of PDB IDs to download or path to a file with the PDB IDs. If None, all available PDB IDs from 'mdcath_source.h5' will be downloaded. 
+            The filters will be applied to the PDB IDs in this list in any case. Default is None.
         min_gyration_radius: float
             Minimum gyration radius (in nm) of the protein structure. Default is None.
         max_gyration_radius: float
@@ -78,143 +75,114 @@ class mdCATH(Dataset):
             Minimum number of frames in the trajectory in order to be considered. Default is None.
         """
 
-        self.url = "https://huggingface.co/datasets/compsciencelab/mdCATH/tree/main/"
+        self.url = "https://huggingface.co/datasets/compsciencelab/mdCATH/resolve/main/"
         self.source_file = "mdcath_source.h5"
-        super(mdCATH, self).__init__(root, transform, pre_transform, pre_filter)
+        self.root = root
+        os.makedirs(root, exist_ok=True)
         self.numAtoms = numAtoms
-        self.numNoHAtoms = numNoHAtoms
         self.numResidues = numResidues
         self.temperatures = temperatures
         self.skipFrames = skipFrames
-        self.pdb_list = get_pdb_list(pdb_list)
+        self.pdb_list = load_pdb_list(pdb_list)
         self.min_gyration_radius = min_gyration_radius
         self.max_gyration_radius = max_gyration_radius
         self.alpha_beta_coil = alpha_beta_coil
         self.numFrames = numFrames
         self.solid_ss = solid_ss
+        self._ensure_source_file()
+        self._filter_and_prepare_data()
         self.idx = None
-        self.process_data_source()
+        super(MDCATH, self).__init__(root, transform, pre_transform, pre_filter)
         # Calculate the total size of the dataset in MB
         self.total_size_mb = self.calculate_dataset_size()
+
         
-        print(f"Total number of domains: {len(self.to_download.keys())}")
-        print(f"Total number of conformers: {self.num_conformers}")
-        print(f"Total size of dataset: {self.total_size_mb} MB")
+        logger.info(f"Total number of domains: {len(self.processed.keys())}")
+        logger.info(f"Total number of conformers: {self.num_conformers}")
+        logger.info(f"Total size of dataset: {self.total_size_mb} MB")
 
     @property
     def raw_file_names(self):
-        # Check if the dataset has been processed, and if not, return the original source file
-        if not hasattr(self, "to_download"):
-            return [self.source_file]
-        # Otherwise, return the list of HDF5 files that passed the filtering criteria
-        return [f"mdcath_dataset_{pdb_id}.h5" for pdb_id in self.to_download.keys()]
+        return [f"mdcath_dataset_{pdb_id}.h5" for pdb_id in self.processed.keys()]
 
     @property
     def raw_dir(self):
-        # Override the raw_dir property to prevent the creation of a 'raw' directory
+        # Override the raw_dir property to return the root directory
         # The files will be downloaded to the root directory
         return self.root
+    
+    def _ensure_source_file(self):
+        """Ensure the source file is downloaded before processing."""
+        source_path = os.path.join(self.root, self.source_file)
+        if not os.path.exists(source_path):
+            logger.info(f"Downloading source file {self.source_file}")
+            urllib.request.urlretrieve(opj(self.url, self.source_file), source_path)
+            
     def download(self):
-        if not hasattr(self, "to_download") or not self.to_download:
-            download_url(opj(self.url, self.source_file), self.root)
-            return
-        for pdb_id in self.to_download.keys():
+        for pdb_id in self.processed.keys():
             file_name = f"mdcath_dataset_{pdb_id}.h5"
-            file_path = opj(self.raw_dir, 'data', file_name)
+            file_path = opj(self.raw_dir, file_name)
             if not os.path.exists(file_path):
-                download_url(opj(self.url, file_name), self.root)
+                # Download the file if it does not exist
+                urllib.request.urlretrieve(opj(self.url, 'data', file_name), file_path)
 
     def calculate_dataset_size(self):
         total_size_bytes = 0
-        for pdb_id in self.to_download.keys():
+        for pdb_id in self.processed.keys():
             file_name = f"mdcath_dataset_{pdb_id}.h5"
             total_size_bytes += os.path.getsize(opj(self.root, file_name))
         total_size_mb = round(total_size_bytes / (1024 * 1024), 4)
         return total_size_mb
-    def process_data_source(self):
-        print("Processing mdCATH source")
-        data_info_path = opj(self.root, self.source_file)
-        if not os.path.exists(data_info_path):
-            self.download()
-        # the to_downlaod is the dictionary that will store the pdb ids and the corresponding temp and replica ids if they pass the filter
-        self.to_download = {}
+    
+    def _filter_and_prepare_data(self):
+        source_info_path = os.path.join(self.root, self.source_file)
+        
+        self.processed = defaultdict(list)
         self.num_conformers = 0
-        with h5py.File(data_info_path, "r") as f:
-            domains = f.keys() if self.pdb_list is None else self.pdb_list
-            for pdb in tqdm(
-                domains, total=len(domains), desc="Processing mdCATH source"
-            ):
-                pdb_group = f[pdb]
-                if (
-                    self.numAtoms is not None
-                    and pdb_group.attrs["numProteinAtoms"] > self.numAtoms
-                ):
+
+        with h5py.File(source_info_path, "r") as file:
+            domains = file.keys() if self.pdb_list is None else self.pdb_list
+            
+            for pdb_id in tqdm(domains, desc="Processing mdcath source"):
+                pdb_group = file[pdb_id]
+                if self.numAtoms is not None and pdb_group.attrs["numProteinAtoms"] > self.numAtoms:
                     continue
-                if (
-                    self.numResidues is not None
-                    and pdb_group.attrs["numResidues"] > self.numResidues
-                ):
+                if self.numResidues is not None and pdb_group.attrs["numResidues"] > self.numResidues:
                     continue
-                if (
-                    self.numNoHAtoms is not None
-                    and pdb_group.attrs["numNoHAtoms"] > self.numNoHAtoms
-                ):
-                    continue
-                for temp in self.temperatures:
-                    if temp not in pdb_group.keys():
-                        continue
-                    for replica in pdb_group[temp].keys():
-                        if (
-                            self.numFrames is not None
-                            and pdb_group[temp][replica].attrs["numFrames"]
-                            < self.numFrames
-                        ):
-                            continue
-                        if (
-                            self.min_gyration_radius is not None
-                            and pdb_group[temp][replica].attrs["min_gyration_radius"]
-                            < self.min_gyration_radius
-                        ):
-                            continue
-                        if (
-                            self.max_gyration_radius is not None
-                            and pdb_group[temp][replica].attrs["max_gyration_radius"]
-                            > self.max_gyration_radius
-                        ):
-                            continue
-                        if (
-                            self.alpha_beta_coil is not None
-                            or self.solid_ss is not None
-                        ):
-                            alpha = pdb_group[temp][replica].attrs["alpha"]
-                            beta = pdb_group[temp][replica].attrs["beta"]
-                            coil = pdb_group[temp][replica].attrs["coil"]
-                            solid_ss = (
-                                (alpha + beta) / pdb_group.attrs["numResidues"] * 100
-                            )
-                            if self.solid_ss is not None:
-                                if solid_ss < self.solid_ss:
-                                    continue
-                            else:
-                                if not np.isclose(
-                                    [alpha, beta, coil], list(self.alpha_beta_coil)
-                                ).all():
-                                    continue
-                                
-                            if pdb not in self.to_download:
-                                self.to_download[pdb] = []
-                            num_frames = math.ceil(pdb_group[temp][replica].attrs["numFrames"]/self.skipFrames)
-                            self.to_download[pdb].append((temp, replica, num_frames))
-                            # append the number of frames of the trajectory to the total number of molecules
-                            self.num_conformers += num_frames
+                self._process_temperatures(pdb_id, pdb_group)
+
+    def _process_temperatures(self, pdb_id, pdb_group):
+        for temp in self.temperatures:
+            for replica in pdb_group[temp].keys():
+                self._evaluate_replica(pdb_id, temp, replica, pdb_group)
+
+    def _evaluate_replica(self, pdb_id, temp, replica, pdb_group):
+        conditions = [
+            self.numFrames is not None and pdb_group[temp][replica].attrs["numFrames"] < self.numFrames,
+            self.min_gyration_radius is not None and pdb_group[temp][replica].attrs["min_gyration_radius"] < self.min_gyration_radius,
+            self.max_gyration_radius is not None and pdb_group[temp][replica].attrs["max_gyration_radius"] > self.max_gyration_radius,
+            self._evaluate_structure(pdb_group, temp, replica)
+        ]
+        if any(conditions):
+            return
+        
+        num_frames = math.ceil(pdb_group[temp][replica].attrs["numFrames"] / self.skipFrames)
+        self.processed[pdb_id].append((temp, replica, num_frames))
+        self.num_conformers += num_frames
+
+    def _evaluate_structure(self, pdb_group, temp, replica):
+        alpha = pdb_group[temp][replica].attrs["alpha"]
+        beta = pdb_group[temp][replica].attrs["beta"]
+        solid_ss = (alpha + beta) / pdb_group.attrs["numResidues"] * 100
+        return self.solid_ss is not None and solid_ss < self.solid_ss
 
     def len(self):
         return self.num_conformers
 
     def _setup_idx(self):
-        files = [opj(self.root, f"mdcath_dataset_{pdb_id}.h5") for pdb_id in self.to_download.keys()]
+        files = [opj(self.root, f"mdcath_dataset_{pdb_id}.h5") for pdb_id in self.processed.keys()]
         self.idx = []
-        for i, (pdb, group_info) in enumerate(self.to_download.items()):
+        for i, (pdb, group_info) in enumerate(self.processed.items()):
             for temp, replica, num_frames in group_info:
                 # build the catalog here for each conformer
                 d = [(pdb, files[i], temp, replica, conf_id) for conf_id in range(num_frames)]
