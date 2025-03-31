02a9eef31f32e45275c5e53648895010a7de0bf7
Stefan Doerr <stefdoerr@gmail.com>
2025-03-31 14:56:02 +0300

    correct 11.8 URL

--- cibuildwheel_support/prepare_build_environment_windows_cu118.sh
+++ cibuildwheel_support/prepare_build_environment_windows_cu118.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8"
-curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_515.60.1_windows.exe
+curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_522.06_windows.exe
 ./cuda.exe -s nvcc_11.8 cudart_11.8 cublas_dev_11.8 curand_dev_11.8 cusparse_dev_11.8 cusolver_dev_11.8
 rm cuda.exe
 
