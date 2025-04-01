c410ab382a026a3073ef47398cce1d8a2050590c
Stefan Doerr <stefdoerr@gmail.com>
2025-04-01 12:55:59 +0300

    missing thrust

--- cibuildwheel_support/prepare_build_environment_windows_cu118.sh
+++ cibuildwheel_support/prepare_build_environment_windows_cu118.sh
@@ -5,7 +5,7 @@ set -x
 
 CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8"
 curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_522.06_windows.exe
-./cuda.exe -s nvcc_11.8 cudart_11.8 cublas_dev_11.8 curand_dev_11.8 cusparse_dev_11.8 cusolver_dev_11.8
+./cuda.exe -s nvcc_11.8 cudart_11.8 cublas_dev_11.8 curand_dev_11.8 cusparse_dev_11.8 cusolver_dev_11.8 thrust_11.8
 rm cuda.exe
 
 export WITH_CUDA=1
