8630dc76d38daf84d3c57a16c92521294dedfd82
Stefan Doerr <stefdoerr@gmail.com>
2025-04-01 19:18:56 +0300

    run from inside container

--- cibuildwheel_support/prepare_build_environment_linux_cpu.sh
+++ cibuildwheel_support/prepare_build_environment_linux_cpu.sh
@@ -1,4 +1,6 @@
 # Configure pip to use PyTorch extra-index-url for CPU
 mkdir -p $HOME/.config/pip
-echo "[global]
-extra-index-url = https://download.pytorch.org/whl/cpu" > $HOME/.config/pip/pip.conf
+echo "[global]\nextra-index-url = https://download.pytorch.org/whl/cpu" > $HOME/.config/pip/pip.conf
+
+
+echo "Platform: $(python -c 'import platform; print(platform.machine())')"
