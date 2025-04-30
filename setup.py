0d317c9199eb38242b0236ae1592266f471d04b2
Stefan Doerr <stefdoerr@gmail.com>
2025-04-30 10:33:13 +0300

    fix rpaths and dependencies for cuda builds

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup
 import torch
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 import os
+import platform
 
 
 # If WITH_CUDA is defined
@@ -42,12 +43,30 @@ neighbor_sources = [
     os.path.join(extension_root, "neighbors", source) for source in neighbor_sources
 ]
 
+runtime_library_dirs = None
+if platform.system() == "Darwin":
+    runtime_library_dirs = [
+        "@loader_path/../../torch/lib",
+        "@loader_path/../../nvidia/cuda_runtime/lib",
+    ]
+elif platform.system() == "Linux":
+    runtime_library_dirs = [
+        "$ORIGIN/../../torch/lib",
+        "$ORIGIN/../../nvidia/cuda_runtime/lib",
+    ]
+
+extra_deps = []
+if use_cuda:
+    cuda_ver = os.getenv("ACCELERATOR", "")[2:4]
+    extra_deps = [f"nvidia-cuda-runtime-cu{cuda_ver}"]
+
 ExtensionType = CppExtension if not use_cuda else CUDAExtension
 extensions = ExtensionType(
     name="torchmdnet.extensions.torchmdnet_extensions",
     sources=[os.path.join(extension_root, "torchmdnet_extensions.cpp")]
     + neighbor_sources,
     define_macros=[("WITH_CUDA", 1)] if use_cuda else [],
+    runtime_library_dirs=runtime_library_dirs,
 )
 
 if __name__ == "__main__":
@@ -58,4 +77,14 @@ if __name__ == "__main__":
                 no_python_abi_suffix=True, use_ninja=False
             )
         },
+        install_requires=[
+            "h5py",
+            # "nnpops",
+            "torch>=2.5",
+            "torch_geometric",
+            "lightning",
+            "tqdm",
+            "numpy",
+        ]
+        + extra_deps,
     )
