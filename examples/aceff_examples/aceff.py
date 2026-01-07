6d6be1efa67637d140b9c79ae3c0df9626ba8919
Stephen Farr <s.farr@acellera.com>
2026-01-07 17:34:06 +0100

    new example (#376)
    * new examples
    
    * actually add the file

--- examples/aceff_examples/aceff.py
+++ examples/aceff_examples/aceff.py
@@ -1,21 +1,20 @@
-# This script shows how to load the AceFF-1.0 checkpoint 
+# This script shows how to load an AceFF checkpoint
 # and use it for force and energy predictions for a single molecule or a batch
 import torch
 
 
-# The AceFF-1.0 model is available from HuggingFace under apache2.0 license 
+# The AceFF models are available from HuggingFace under apache2.0 license
 from huggingface_hub import hf_hub_download
 
 model_file_path = hf_hub_download(
-    repo_id="Acellera/AceFF-2.0",
-    filename="aceff_v2.0.ckpt"
+    repo_id="Acellera/AceFF-2.0", filename="aceff_v2.0.ckpt"
 )
 
 print("Downloaded to:", model_file_path)
 
 from torchmdnet.models.model import load_model
-model = load_model(model_file_path, derivative=True)
 
+model = load_model(model_file_path, derivative=True)
 
 
 # single molecule
@@ -32,6 +31,3 @@ batch = torch.tensor([0, 0, 0, 1, 1, 1], dtype=torch.long)
 energies, forces = model(z, pos, batch)
 print(energies)
 print(forces)
-
-
-
